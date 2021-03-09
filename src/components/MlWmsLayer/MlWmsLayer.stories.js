d8a2d261a23abd441defbee079e7eba6d24d8b52
cioddi <maxtobiasweber@gmail.com>
2021-03-09 11:18:13 +0100

    Add MlWmsLayer - simple WMS Layer component that accepts a WMS URL and layer name as props

---
+++ src/components/MlWmsLayer/MlWmsLayer.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import MlWmsLayer from "./MlWmsLayer";
+import TopToolbar from "../../ui_components/TopToolbar";
+
+import "../../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  title: "MapComponents/MlWmsLayer",
+  component: MlWmsLayer,
+  argTypes: {
+    url: {},
+    layer: {},
+  },
+};
+
+const Template = (args) => (
+  <TopToolbar>
+    <MlWmsLayer
+      url={args.url}
+      layer={args.layer}
+      sourceOptions={args.sourceOptions}
+    />
+  </TopToolbar>
+);
+
+export const ExampleConfig = Template.bind({});
+ExampleConfig.parameters = {};
+ExampleConfig.args = {
+  url: "https://www.wms.nrw.de/geobasis/wms_nw_uraufnahme",
+  layer: "nw_uraufnahme_rw",
+  sourceOptions: {
+    minzoom: 13,
+    maxzoom: 20,
+  },
+};
+//
