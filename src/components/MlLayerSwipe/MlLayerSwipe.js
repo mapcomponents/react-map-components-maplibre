a1e3fa6acd6eb73d699d21535b0da45986a1ddcd
cioddi <maxtobiasweber@gmail.com>
2021-03-11 17:57:43 +0100

    Disable global decorator and add specific decorator selection inside of the stories

---
+++ src/components/MlLayerSwipe/MlLayerSwipe.js
@@ -0,0 +1,89 @@
+import React, { useContext, useRef, useEffect, useState } from "react";
+import { MapContext } from "react-map-components-core";
+
+import Button from "@material-ui/core/Button";
+
+/**
+ * MlLayerSwipe returns a Button that will add a standard OSM tile layer to the maplibre-gl instance.
+ */
+const MlLayerSwipe = (props) => {
+  const mapContext = useContext(MapContext);
+
+  const [showLayer, setShowLayer] = useState(true);
+  const idPostfixRef = useRef(new Date().getTime());
+
+  const cleanup = () => {
+    if (mapContext.map.getLayer("raster-tile-layer-" + idPostfixRef.current)) {
+      mapContext.map.removeLayer("raster-tile-layer-" + idPostfixRef.current);
+    }
+    if (mapContext.map.getSource("raster-tile-source-" + idPostfixRef.current)) {
+      mapContext.map.removeSource("raster-tile-source-" + idPostfixRef.current);
+    }
+  };
+
+  useEffect(() => {
+    if (!mapContext.map) return;
+
+    return () => {
+      cleanup();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!mapContext.map) return;
+
+    cleanup();
+
+    // Add the new layer to the openlayers instance once it is available
+    //    mapContext.map.addSource("raster-tile-source-" + idPostfixRef.current, {
+    //      type: "raster",
+    //      tiles: [
+    //        props.url +
+    //          "?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&width=256&height=256&layers=" +
+    //          props.layer,
+    //      ],
+    //      tileSize: 256,
+    //      attribution: "",
+    //      //...props.sourceOptions,
+    //    });
+    //    mapContext.map.addLayer({
+    //      id: "raster-tile-layer-" + idPostfixRef.current,
+    //      type: "raster",
+    //      source: "raster-tile-source-" + idPostfixRef.current,
+    //      minzoom: 0,
+    //      maxzoom: 10,
+    //      ...props.sourceOptions,
+    //    });
+  }, [mapContext.map]);
+
+  useEffect(() => {
+    if (!mapContext.map) return;
+
+    // toggle layer visibility by changing the layout object's visibility property
+    if (showLayer) {
+      mapContext.map.setLayoutProperty(
+        "raster-tile-layer-" + idPostfixRef.current,
+        "visibility",
+        "visible"
+      );
+    } else {
+      mapContext.map.setLayoutProperty(
+        "raster-tile-layer-" + idPostfixRef.current,
+        "visibility",
+        "none"
+      );
+    }
+  }, [showLayer]);
+
+  return (
+    <Button
+      color="primary"
+      variant={showLayer ? "contained" : "outlined"}
+      onClick={() => setShowLayer(!showLayer)}
+    >
+      WMS
+    </Button>
+  );
+};
+
+export default MlLayerSwipe;
---
+++ src/components/MlLayerSwipe/MlLayerSwipe.stories.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import MlLayerSwipe from "./MlLayerSwipe";
+import TopToolbar from "../../ui_components/TopToolbar";
+
+import { MapComponentsProvider } from "react-map-components-core";
+
+import MapLibreMap from "../MapLibreMap/MapLibreMap";
+
+import multiMapContextDecorator from "../../decorators/MultiMapContextDecorator";
+
+import "../../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  title: "MapComponents/MlLayerSwipe",
+  component: MlLayerSwipe,
+  argTypes: {
+    url: {},
+    layer: {},
+  },
+  decorators: multiMapContextDecorator,
+};
+
+const Template = (args) => (
+  <TopToolbar>
+    <MlLayerSwipe
+      url={args.url}
+      layer={args.layer}
+      sourceOptions={args.sourceOptions}
+    />
+  </TopToolbar>
+);
+
+export const ExampleConfig = Template.bind({});
+ExampleConfig.parameters = {};
+ExampleConfig.args = {
+  url: "https://www.wms.nrw.de/geobasis/wms_nw_uraufnahme",
+  layer: "nw_uraufnahme_rw",
+  sourceOptions: {
+    minzoom: 13,
+    maxzoom: 20,
+  },
+};
+//
