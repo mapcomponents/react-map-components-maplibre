220fb42661982c87271fb2bff8caa480daece73c
cioddi <maxtobiasweber@gmail.com>
2021-03-16 17:06:26 +0100

    MlLayerSwipe & MlLayerMagnify (WIP)

---
+++ src/components/MlLayerMagnify/MlLayerMagnify.js
@@ -0,0 +1,77 @@
+import React, { useContext, useRef, useEffect, useState } from "react";
+import compare from "./utils/compareMagnify";
+import "./style.css";
+import { MapContext } from "react-map-components-core";
+
+import Button from "@material-ui/core/Button";
+
+/**
+ *
+ * MlLayerMagnify returns a Button that will add a standard OSM tile layer to the maplibre-gl instance.
+ */
+const MlLayerMagnify = (props) => {
+  const mapContext = useContext(MapContext);
+
+  const [showLayer, setShowLayer] = useState(true);
+  const compareRef = useRef(null);
+
+  const mapExists = () => {
+    if (!props.map1Id || !props.map2Id) {
+      return false;
+    }
+    if (
+      mapContext.mapIds.indexOf(props.map1Id) === -1 ||
+      mapContext.mapIds.indexOf(props.map2Id) === -1
+    ) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const cleanup = () => {
+    console.log(compareRef.current);
+    if (compareRef.current) {
+      console.log("remove");
+      compareRef.current.remove();
+      compareRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    if (!mapExists()) return;
+
+    return cleanup;
+  }, []);
+
+  useEffect(() => {
+    if (!mapExists()) return;
+
+    if (!compareRef.current) {
+      //clip-path: circle(60px at 50% 50%);
+      compareRef.current = new compare(
+        mapContext.maps[props.map1Id],
+        mapContext.maps[props.map2Id],
+        ".maps",
+        {
+          // Set this to enable comparing two maps by mouse movement:
+          // mousemove: true
+        }
+      );
+
+      window.compare = compareRef.current;
+    }
+  }, [mapContext.mapIds]);
+
+  return (
+    <Button
+      color="primary"
+      variant={showLayer ? "contained" : "outlined"}
+      onClick={() => setShowLayer(!showLayer)}
+    >
+      Layer Swipe
+    </Button>
+  );
+};
+
+export default MlLayerMagnify;
---
+++ src/components/MlLayerMagnify/MlLayerMagnify.stories.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import MlLayerMagnify from "./MlLayerMagnify";
+import MlWmsLayerMulti from "../MlWmsLayerMulti/MlWmsLayerMulti";
+import TopToolbar from "../../ui_components/TopToolbar";
+
+import multiMapContextDecorator from "../../decorators/MultiMapContextDecorator";
+
+import "../../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default {
+  title: "MapComponents/MlLayerMagnify",
+  component: MlLayerMagnify,
+  argTypes: {
+    url: {},
+    layer: {},
+  },
+  decorators: multiMapContextDecorator,
+};
+
+const Template = (args) => (
+  <TopToolbar>
+    <MlWmsLayerMulti
+      url="https://www.wms.nrw.de/geobasis/wms_nw_uraufnahme"
+      layer="nw_uraufnahme_rw"
+      sourceOptions={{
+        minzoom: 13,
+        maxzoom: 20,
+      }}
+      mapId="map_1"
+    />
+    <MlLayerMagnify map1Id="map_1" map2Id="map_2" />
+  </TopToolbar>
+);
+
+export const ExampleConfig = Template.bind({});
+ExampleConfig.parameters = {};
+ExampleConfig.args = {};
+//
