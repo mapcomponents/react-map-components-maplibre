fd679d2d1d6b9925b21efe9baf3441935c875020
cioddi <maxtobiasweber@gmail.com>
2021-03-24 08:27:35 +0100

    Split storybook build into chunks to prevent out of memory bug when building

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,16 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
   ],
+  webpackFinal: async (config, { configType }) => {
+    // split into more chunks
+    config.optimization = {
+      splitChunks: {
+        chunks: "all",
+        minSize: 30 * 1024, // 30KB
+        maxSize: 1024 * 1024, // 1MB
+      },
+    };
+
+    return config;
+  },
 };
--- webpack.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = async ({ config, mode }) => {
-  config.optimization.minimize = false;
-};
