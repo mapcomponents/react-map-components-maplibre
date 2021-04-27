b47fe9dffb113cb4bb42699a5dbd72b58fd48259
David Patzke <david.patzke@wheregroup.com>
2021-04-27 16:25:06 +0200

    Adjust code to use Context for data

--- src/components/MlWanderApp/MlWanderApp.js
+++ src/components/MlWanderApp/MlWanderApp.js
@@ -6,32 +6,34 @@ import MlHillshadeLayer from "../MlHillshadeLayer/MlHillshadeLayer";
 import MlLayerMagnify from "../MlLayerMagnify/MlLayerMagnify";
 import MlWmsLayerMulti from "../MlWmsLayerMulti/MlWmsLayerMulti";
 import MlGPXViewer from "../MlGPXViewer/MlGPXViewer";
-
-
-
-
+import GeoJsonProvider from "../MlGPXViewer/util/GeoJsonProvider";
+import MlSpatialElevationProfile from "../MlSpatialElevationProfile/MlSpatialElevationProfile";
 
 const MlWanderApp = (props) => {
- 
-  const wmsLayerMapId  = props.wmsLayerMapId;
-  
-  return <>
- 
-  <TopToolbar>
-  
-    <MlWmsLayerMulti
-    url="https://www.wms.nrw.de/geobasis/wms_nw_dop?language=ger&bbox={bbox-epsg-3857}"
-    layer="nw_dop_rgb"
-    sourceOptions={{
-      maxzoom: 24  
-    }}
-    mapId="map_2"
-  />
-  </TopToolbar>
-  <MlGPXViewer mapId="map_1"/ >
-  <MlLayerMagnify map1Id="map_1" map2Id="map_2"  magnifierRadius={20}/>
-  <MlHillshadeLayer />
-  </>;
+  const wmsLayerMapId = props.wmsLayerMapId;
+
+  return (
+    <>
+      <TopToolbar>
+        <MlWmsLayerMulti
+          url="https://www.wms.nrw.de/geobasis/wms_nw_dop?language=ger&bbox={bbox-epsg-3857}"
+          layer="nw_dop_rgb"
+          sourceOptions={{
+            maxzoom: 24,
+          }}
+          mapId="map_2"
+        />
+      </TopToolbar>
+      <MlGPXViewer mapId="map_1" />
+
+      <GeoJsonProvider>
+        <MlGPXViewer mapId="map_1" />
+      
+      </GeoJsonProvider>
+      <MlLayerMagnify map1Id="map_1" map2Id="map_2" magnifierRadius={20} />
+      <MlHillshadeLayer />
+    </>
+  );
 };
 
 export default MlWanderApp;
