8a18cac825ced99d1fe3e61db2ae0d5dd9ae91a5
cioddi <maxtobiasweber@gmail.com>
2021-04-30 15:26:36 +0200

    Add LoadingOverlay to DemoDashboardDecorator

--- src/decorators/MapContextDashboardDecorator.js
+++ src/decorators/MapContextDashboardDecorator.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import { MapComponentsProvider } from "react-map-components-core";
-
+import { LoadingOverlayProvider } from "../ui_components/LoadingOverlayContext";
+import LoadingOverlay from "../ui_components/LoadingOverlay";
 
 const decorators = [
   (Story) => (
     <MapComponentsProvider>
-      <Story />
-     
+      <LoadingOverlayProvider>
+        <LoadingOverlay></LoadingOverlay>
+        <Story />
+      </LoadingOverlayProvider>
     </MapComponentsProvider>
   ),
 ];
