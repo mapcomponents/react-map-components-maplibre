a5cda4dd66e0cc8eefc5ea7cc1fdd624a9c46329
cioddi <maxtobiasweber@gmail.com>
2021-05-16 11:51:31 +0200

    Add Laufwettbewerb daily prgress chart - WIP

---
+++ src/components/MlLaufwettbewerbApp/assets/DailyProgressChart.js
@@ -0,0 +1,101 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { ResponsiveLine } from "@nivo/line";
+
+function DailyProgressChart(props) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!props.data) return;
+    console.log(props.data);
+
+    let dateKeys = Object.keys(props.data);
+    let data = [];
+    for (var i = 0, len = dateKeys.length; i < len; i++) {
+      data.push({
+        y: props.data[dateKeys[i]],
+        x: dateKeys[i],
+      });
+    }
+
+    console.log(data);
+    setData([
+      {
+        id: "Tagesfortschritt",
+        color: "hsl(45, 70%, 50%)",
+        data: data,
+      },
+    ]);
+  }, [props.data]);
+
+  return (
+    <>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: "point" }}
+        yScale={{
+          type: "linear",
+          min: "auto",
+          max: "auto",
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: "bottom",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "transportation",
+          legendOffset: 36,
+          legendPosition: "middle",
+        }}
+        axisLeft={{
+          orient: "left",
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "count",
+          legendOffset: -40,
+          legendPosition: "middle",
+        }}
+        pointSize={10}
+        pointColor={{ theme: "background" }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: "serieColor" }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: "left-to-right",
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: "circle",
+            symbolBorderColor: "rgba(0, 0, 0, .5)",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemBackground: "rgba(0, 0, 0, .03)",
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </>
+  );
+}
+
+export default DailyProgressChart;
