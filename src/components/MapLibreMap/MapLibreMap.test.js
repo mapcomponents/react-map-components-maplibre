f1ce55fc45f18ab1875c6067591d32ab6d06bac9
Tobias Weber <tobias.weber@wheregroup.com>
2021-09-08 13:49:03 +0200

    Add some MapLibreMap tests

--- src/components/MapLibreMap/MapLibreMap.test.js
+++ src/components/MapLibreMap/MapLibreMap.test.js
@@ -1,17 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { mount, configure } from "enzyme";
 import { waitFor } from "@testing-library/react";
-import maplibregl from "maplibre-gl";
 import { MapContext, MapComponentsProvider } from "react-map-components-core";
 import MapLibreMap from "./MapLibreMap";
 
-const MapLibreMapTestComponent = () => {
+const MapLibreMapTestComponent = (props) => {
   const mapContext = useContext(MapContext);
+  const [mapIsVisible, setMapIsVisible] = useState(true);
 
   return (
     <>
+      <button
+        className="toggle_map_is_visible"
+        onClick={() => {
+          setMapIsVisible(!mapIsVisible);
+        }}
+      >
+        toggle mapIsVisible
+      </button>
       <div className="map_count">{mapContext.mapIds.length}</div>
-      <MapLibreMap />
+
+      {!props.mapId && mapIsVisible && <MapLibreMap />}
+      {props.mapId && (
+        <>
+          <div className="map_1_exists">
+            {mapContext.getMap(props.mapId) ? "true" : "false"}
+          </div>
+          {mapIsVisible && <MapLibreMap mapId={props.mapId} />}
+        </>
+      )}
     </>
   );
 };
@@ -24,8 +41,44 @@ describe("<MapLibreMap>", () => {
       </MapComponentsProvider>
     );
 
-    await waitFor(() => expect(wrapper.find(".map_count").text()).toEqual("1"));
+    expect(wrapper.find(".map_count").text()).toEqual("1");
+  });
+
+  it("should remove an anonymous maplibre object from mapContext", async () => {
+    const wrapper = mount(
+      <MapComponentsProvider>
+        <MapLibreMapTestComponent />
+      </MapComponentsProvider>
+    );
 
     expect(wrapper.find(".map_count").text()).toEqual("1");
+
+    wrapper.find(".toggle_map_is_visible").simulate("click");
+
+    expect(wrapper.find(".map_count").text()).toEqual("0");
+  });
+
+  it("should register a maplibre object with the id 'map_1' to mapContext", async () => {
+    const wrapper = mount(
+      <MapComponentsProvider>
+        <MapLibreMapTestComponent mapId="map_1" />
+      </MapComponentsProvider>
+    );
+
+    expect(wrapper.find(".map_1_exists").text()).toEqual("true");
+  });
+
+  it("should remove a maplibre object with the id 'map_1' to mapContext", async () => {
+    const wrapper = mount(
+      <MapComponentsProvider>
+        <MapLibreMapTestComponent mapId="map_1" />
+      </MapComponentsProvider>
+    );
+
+    expect(wrapper.find(".map_1_exists").text()).toEqual("true");
+
+    wrapper.find(".toggle_map_is_visible").simulate("click");
+
+    expect(wrapper.find(".map_1_exists").text()).toEqual("false");
   });
 });
