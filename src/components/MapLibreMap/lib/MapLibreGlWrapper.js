b2369e237e232faa7301ce80ac7645013a92e968
Tobias Weber <tobias.weber@wheregroup.com>
2021-09-27 13:32:45 +0200

    Add sample implementation of MapLibreGlWrapper controlled cleanup function

--- src/components/MapLibreMap/lib/MapLibreGlWrapper.js
+++ src/components/MapLibreMap/lib/MapLibreGlWrapper.js
@@ -2,6 +2,61 @@ const MapLibreGlWrapper = function (props) {
   let self = this;
   this.map = props.map;
 
+  this.registeredElements = {};
+
+  this.initRegisteredElements = (componentId) => {
+    if (typeof self.registeredElements[componentId] === "undefined") {
+      self.registeredElements[componentId] = {
+        layer: [],
+        source: [],
+        image: [],
+        event: [],
+      };
+    }
+  };
+
+  this.on = (type, layerId, listener, componentId) => {
+    if (typeof listener === "string" && typeof layerId === "function") {
+      return self.on.call(self, type, undefined, layerId, listener);
+    }
+    if (componentId && typeof componentId === "string") {
+      self.initRegisteredElements(componentId);
+      self.registeredElements[componentId].events.push([type, layerId, listener]);
+    }
+
+    self.map.on(type, layerId, listener);
+  };
+
+  this.cleanup = (componentId) => {
+    if (typeof self.registeredElements[componentId] !== "undefined") {
+      // cleanup layers
+      self.registeredElements[componentId].layer.forEach((item) => {
+        if (self.map.getLayer(item)) {
+          self.map.removeLayer(item);
+        }
+      });
+
+      // cleanup sources
+      self.registeredElements[componentId].source.forEach((item) => {
+        if (self.map.getSource(item)) {
+          self.map.removeSource(item);
+        }
+      });
+
+      // cleanup images
+      self.registeredElements[componentId].image.forEach((item) => {
+        if (self.map.hasImage(item)) {
+          self.map.removeImage(item);
+        }
+      });
+
+      // cleanup events
+      self.registeredElements[componentId].event.forEach((item) => {
+        self.map.off(...item);
+      });
+    }
+  };
+
   // add style prop functions that require map._update to be called afterwards
   let updatingStyleFunctions = [
     "addLayer",
