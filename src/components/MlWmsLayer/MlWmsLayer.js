e647de8ab362d8c2a51c4875a6f7adc978194eed
Tobias Weber <tobias.weber@wheregroup.com>
2021-10-14 22:22:03 +0200

    Add jsdoc param annotations to get vscode intellisense working

--- src/components/MlWmsLayer/MlWmsLayer.js
+++ src/components/MlWmsLayer/MlWmsLayer.js
@@ -25,9 +25,21 @@ const defaultProps = {
     maxZoom: 20,
   },
 };
+
 /**
- * MlWmsLayer WMS raster layer to the maplibre-gl instance.
- *
+ * Adds a WMS raster layer to the maplibre-gl instance
+ * 
+ * @param {object} props
+ * @param {object} props.urlParameters URL query parameters that will be added to the WMS URL. A layers property (string) is mandatory. Any value defined on this attribute will extend the default object
+ * @param {string} props.url WMS URL
+ * @param {bool} props.visible Sets layer "visibility" property to "visible" if true or "none" if false
+ * @param {string} props.attribution MapLibre attribution shown in the bottom right of the map, if this layer is visible
+ * @param {string} props.mapId Id of the target MapLibre instance in mapContext
+ * @param {object} props.sourceOptions Object that is passed to the MapLibre.addSource call as config option parameter
+ * @param {object} props.layerOptions Object that is passed to the MapLibre.addLayer call as config option parameter
+ * @param {string} props.insertBeforeLayer Id of an existing layer in the mapLibre instance to help specify the layer order
+                                           This layer will be visually beneath the layer with the "insertBeforeLayer" id
+ * 
  * @component
  */
 const MlWmsLayer = (props) => {
@@ -113,7 +125,7 @@ MlWmsLayer.defaultProps = {
 
 MlWmsLayer.propTypes = {
   /**
-   * WMS URL.
+   * WMS URL
    */
   url: PropTypes.string.isRequired,
   /**
@@ -131,24 +143,29 @@ MlWmsLayer.propTypes = {
     height: PropTypes.number,
   }),
   /**
-   * Id of the target MapLibre instance in mapContext.
+   * Id of the target MapLibre instance in mapContext
    */
   mapId: PropTypes.string,
   /**
-   * MapLibre attribution shown in the bottom right of the map, if this layer is visible.
+   * MapLibre attribution shown in the bottom right of the map, if this layer is visible
    */
   attribution: PropTypes.string,
   /**
-   * Object that is passed to the MapLibre.addLayer call as config option parameter.
+   * Object that is passed to the MapLibre.addLayer call as config option parameter
    */
   layerOptions: PropTypes.object,
   /**
-   * Object that is passed to the MapLibre.addSource call as config option parameter.
+   * Object that is passed to the MapLibre.addSource call as config option parameter
    */
   sourceOptions: PropTypes.object,
   /**
-   * Id of an existing layer in the mapLibre instance to help specify the layer order. This layer will be visually beneath the layer with the "insertBeforeLayer" id.
+   * Id of an existing layer in the mapLibre instance to help specify the layer order
+   * This layer will be visually beneath the layer with the "insertBeforeLayer" id.
    */
-  insertBeforLayer: PropTypes.string,
+  insertBeforeLayer: PropTypes.string,
+  /**
+   * Sets layer "visibility" property to "visible" if true or "none" if false
+   */
+  visible: PropTypes.bool,
 };
 export default MlWmsLayer;
