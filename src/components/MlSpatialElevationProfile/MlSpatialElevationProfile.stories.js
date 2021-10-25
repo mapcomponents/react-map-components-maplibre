44a378ecaa5940c7ca4e56ede7c0913806a89a16
Nils Kaster <nils.kaster@wheregroup.com>
2021-10-25 09:46:50 +0200

    add sample.gpx file to assets add Download Button for sample.gpx

--- src/components/MlSpatialElevationProfile/MlSpatialElevationProfile.stories.js
+++ src/components/MlSpatialElevationProfile/MlSpatialElevationProfile.stories.js
@@ -5,6 +5,10 @@ import MlGPXViewer from "../MlGPXViewer/MlGPXViewer";
 
 import mapContextDecorator from "../../decorators/MapContextDecorator";
 import GeoJsonProvider from "../MlGPXViewer/util/GeoJsonProvider";
+import Button from "@mui/material/Button";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import InfoIcon from "@mui/icons-material/Info";
+import IconButton from "@mui/material/IconButton";
 
 const storyoptions = {
   title: "MapComponents/MlSpatialElevationProfile",
@@ -22,9 +26,25 @@ export default storyoptions;
 
 const Template = (args) => (
   <>
+    <IconButton
+      style={{
+        position: "absolute",
+        right: "5px",
+        bottom: "125px",
+        backgroundColor: "rgba(255,255,255,1)",
+
+        zIndex: 1000,
+      }}
+      title="Download sample-data"
+      size="large"
+      href="/assets/sample.gpx"
+      target="blank"
+    >
+      <FileDownloadIcon/>
+    </IconButton>
     <GeoJsonProvider>
-      <MlGPXViewer />
-      <MlSpatialElevationProfile />
+      <MlGPXViewer/>
+      <MlSpatialElevationProfile/>
     </GeoJsonProvider>
   </>
 );
