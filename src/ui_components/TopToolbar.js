6ad469ac9c6dcdf076d64bf6569be831d440a2f3
Max Tobias Weber <tobias.weber@wheregroup.com>
2021-12-12 10:52:28 +0100

    Fix @mui TopToolbar colors

--- src/ui_components/TopToolbar.js
+++ src/ui_components/TopToolbar.js
@@ -3,27 +3,27 @@ import makeStyles from "@mui/styles/makeStyles";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    zIndex: 120,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-  },
-  AppBar: {
-    backgroundColor: "#fafafa",
-    minHeight: "62px",
-  },
-}));
 
 export default function TopToolbar(props) {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar className={classes.AppBar} position="static">
+    <div
+      style={{
+        flexGrow: 1,
+        zIndex: 120,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <AppBar
+        sx={{
+          minHeight: "62px",
+          backgroundColor: "#f1f1f1",
+        }}
+        color="primary"
+        position="static"
+      >
         <Toolbar>{props.children}</Toolbar>
       </AppBar>
     </div>
