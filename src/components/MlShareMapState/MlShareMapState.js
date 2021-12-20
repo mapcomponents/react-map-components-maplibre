1a894701e0c364dc6a69a45afca31281537bbccd
Nils Kaster <nils.kaster@wheregroup.com>
2021-12-20 14:29:29 +0100

    add UrlParameters to save Layer params for MlShareMapState

--- src/components/MlShareMapState/MlShareMapState.js
+++ src/components/MlShareMapState/MlShareMapState.js
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect, useContext, useState } from "react";
+import React, {useRef, useEffect, useContext, useState} from "react";
 import PropTypes from "prop-types";
 
-import { MapContext } from "@mapcomponents/react-core";
-import { v4 as uuidv4 } from "uuid";
+import {MapContext} from "@mapcomponents/react-core";
+import {v4 as uuidv4} from "uuid";
+import useMapState from "../../hooks/useMapState";
 
 /**
  * TODO: Add short & useful description
@@ -15,11 +16,21 @@ import { v4 as uuidv4 } from "uuid";
 const MlShareMapState = (props) => {
   // Use a useRef hook to reference the layer object to be able to access it later inside useEffect hooks
   const mapContext = useContext(MapContext);
-
   const initializedRef = useRef(false);
   const mapRef = useRef(undefined);
   const [map, setMap] = useState(undefined);
   const componentId = useRef((props.idPrefix ? props.idPrefix : "MlShareMapState-") + uuidv4());
+  const [isInitialState, setIsInitialState] = useState(true);
+  const mapState = useMapState({
+    watch: {
+      viewport: false,
+      layers: true,
+      sources: false
+    },
+    filter: {
+      includeBaseLayers: false
+    }
+  })
 
   const mapStateRef = useRef({});
 
@@ -49,32 +60,59 @@ const MlShareMapState = (props) => {
     mapRef.current = mapContext.getMap(props.mapId);
     setMap(mapRef.current);
 
-    let currentUrlParams = getCurrentUrlParameters();
+    const currentUrlParams = getCurrentUrlParameters();
     if (currentUrlParams.lat && currentUrlParams.lng) {
       mapStateRef.current.lat = currentUrlParams.lat;
       mapStateRef.current.lng = currentUrlParams.lng;
       mapStateRef.current.zoom = currentUrlParams.zoom;
-      mapRef.current.setCenter([mapStateRef.current.lng, mapStateRef.current.lat]);
       mapRef.current.setZoom(mapStateRef.current.zoom);
     }
   }, [mapContext.mapIds, mapContext, props.mapId, props.active]);
 
   useEffect(() => {
     if (!map) return;
+    if(!mapState.layers) return;
+    if(!isInitialState) return;
+
+    const currentUrlParams = getCurrentUrlParameters()
+
+    if(currentUrlParams.layers) {
+      for (let x in currentUrlParams.layers) {
+        mapRef.current.getLayer(currentUrlParams.layers[x].id).visibility = currentUrlParams.layers[x].visible ? "visible" : "none"
+        mapRef.current.getLayer(currentUrlParams.layers[x].id).type = currentUrlParams.layers[x].type
+      }
+    }
+
+  }, [mapState.layers, props.mapId, props.active])
+
+  useEffect(() => {
+    if (!map) return;
+    if (!mapState.layers) return;
 
     if (props.active) {
+      setIsInitialState(false)
       map.on(
         "moveend",
         () => {
+          let mapLayers = []
+          for (let x in mapState.layers) {
+            mapLayers.push(new URLSearchParams({
+              id: mapState.layers[x].id,
+              type: mapState.layers[x].type,
+              visible: mapState.layers[x].visible
+            }))
+          }
           refreshMapState();
           let urlParams = new URLSearchParams({
             ...getCurrentUrlParameters(),
             ...mapStateRef.current,
+            layers : mapLayers
           });
+
           let currentParams = new URLSearchParams(window.location.search);
           if (urlParams.toString() !== currentParams.toString()) {
             window.history.pushState(
-              { ...mapStateRef.current },
+              {...mapStateRef.current},
               document.title,
               "?" + urlParams.toString()
             );
@@ -88,7 +126,33 @@ const MlShareMapState = (props) => {
   }, [props.active, map]);
 
   const getCurrentUrlParameters = () => {
-    return Object.fromEntries(new URLSearchParams(window.location.search));
+    let parameterObject = Object.fromEntries(new URLSearchParams(window.location.search))
+
+    if(window.location.search.indexOf("layers")!==-1) {
+      let layerParamString = window.location.search.substring(window.location.search.indexOf("layers"))
+      layerParamString = layerParamString.substring(0, (layerParamString.indexOf("&")!==-1) ? layerParamString.indexOf("&") : layerParamString.length)
+      parameterObject = Object.fromEntries(new URLSearchParams(window.location.search.replace(layerParamString, "")))
+      let layerParams = layerParamString.substring(7)
+      layerParams = layerParams.replaceAll("%3D", "=")
+      layerParams = layerParams.replaceAll("%26", "&")
+      layerParams = layerParams.replaceAll("%2C", ",")
+
+      if (layerParams.indexOf(",")) {
+        layerParams = layerParams.split(",")
+      } else {
+        layerParams = [layerParams]
+      }
+
+      for (let x in layerParams) {
+        let layerState = layerParams[x].split("&")
+        layerParams[x] = {}
+        for (let y in layerState) {
+          layerParams[x][layerState[y].split("=")[0]] = layerState[y].split("=")[1]
+        }
+      }
+      parameterObject["layers"] = layerParams
+    }
+    return parameterObject
   };
 
   const refreshMapState = () => {
@@ -99,7 +163,7 @@ const MlShareMapState = (props) => {
 
   window.onpopstate = (event) => {
     if (event.state && event.state.lng && event.state.lat && event.state.zoom) {
-      mapRef.current.easeTo({ zoom: event.state.zoom, center: [event.state.lng, event.state.lat] });
+      mapRef.current.easeTo({zoom: event.state.zoom, center: [event.state.lng, event.state.lat]});
     }
   };
 
--- src/components/MlShareMapState/MlShareMapState.stories.js
+++ src/components/MlShareMapState/MlShareMapState.stories.js
@@ -2,6 +2,8 @@ import React, { useState} from "react";
 
 import MlShareMapState from "./MlShareMapState";
 import mapContextDecorator from "../../decorators/MapContextDecorator";
+import useMapState from "../../hooks/useMapState";
+import MlLayer from "../MlLayer/MlLayer";
 
 const storyoptions = {
   title: "MapComponents/MlShareMapState",
@@ -12,7 +14,19 @@ const storyoptions = {
 export default storyoptions;
 
 const Template = (args) => {
-  const [watchState, setWatchState] = useState(true);
+  const [watchState, setWatchState] = useState(false);
+  const [testLayerVisible, setTestLayerVisible] = useState(true);
+  const mapState = useMapState({
+      watch : {
+        viewport : false,
+        layers : true,
+        sources : false
+      },
+      filter : {
+        includeBaseLayers : false
+      }
+    }
+  )
 
   return (
     <>
@@ -22,7 +36,16 @@ const Template = (args) => {
       >
         watch map state {watchState?1:0}
       </button>
+      <button
+        style={{ zIndex: "1000", position: "absolute" }}
+        onClick={() => setTestLayerVisible(!testLayerVisible)}
+      >
+        visibility {testLayerVisible?1:0}
+      </button>
       <MlShareMapState active={watchState} />
+      <MlLayer layerId={"MlLayer-testLayer"} options={{layout: {visibility: (testLayerVisible ? "visible":"none")}}} />
+      <MlLayer layerId={"MlLayer-testLayer2"} options={{layout: {visibility: (testLayerVisible ? "visible":"none")}}} />
+      <MlLayer layerId={"MlLayer-testLayer3"} options={{layout: {visibility: (testLayerVisible ? "visible":"none")}}} />
     </>
   );
 };
