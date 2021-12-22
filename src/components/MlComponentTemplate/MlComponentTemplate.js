c00e96c622aaf8d39bf63a2442ac9b34b7819a64
Nils Kaster <nils.kaster@wheregroup.com>
2021-12-22 09:45:01 +0100

    Refactor MlComponentTemplate to make use of the useMap hook

--- src/components/MlComponentTemplate/MlComponentTemplate.js
+++ src/components/MlComponentTemplate/MlComponentTemplate.js
@@ -1,8 +1,6 @@
 import React, { useRef, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-
-import { MapContext } from "@mapcomponents/react-core";
-import { v4 as uuidv4 } from "uuid";
+import useMap from "../../hooks/useMap";
 
 /**
  * TODO: Add short & useful description
@@ -13,40 +11,17 @@ import { v4 as uuidv4 } from "uuid";
  * @component
  */
 const MlComponentTemplate = (props) => {
-  // Use a useRef hook to reference the layer object to be able to access it later inside useEffect hooks
-  const mapContext = useContext(MapContext);
-
+  const mapHook = useMap({mapId: props.mapId, waitForLayer: props.insertBeforeLayer});
   const initializedRef = useRef(false);
-  const mapRef = useRef(undefined);
-  const componentId = useRef((props.idPrefix ? props.idPrefix : "MlComponentTemplate-") + uuidv4());
 
   useEffect(() => {
-    let _componentId = componentId.current;
-
-    return () => {
-      // This is the cleanup function, it is called when this react component is removed from react-dom
-      // try to remove anything this component has added to the MapLibre-gl instance
-      // e.g.: remove the layer
-      // mapContext.getMap(props.mapId).removeLayer(layerRef.current);
-      // check for the existence of map.style before calling getLayer or getSource
-
-      if (mapRef.current) {
-        mapRef.current.cleanup(_componentId);
-        mapRef.current = undefined;
-      }
-      initializedRef.current = false;
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!mapContext.mapExists(props.mapId) || initializedRef.current) return;
-    // the MapLibre-gl instance (mapContext.getMap(props.mapId)) is accessible here
+    if (!mapHook.mapIsReady || initializedRef.current) return;
+    // the MapLibre-gl instance (mapHook.map) is accessible here
     // initialize the layer and add it to the MapLibre-gl instance or do something else with it
     initializedRef.current = true;
-    mapRef.current = mapContext.getMap(props.mapId);
 
-    mapRef.current.setCenter([7.132122000552613, 50.716405378037706]);
-  }, [mapContext.mapIds, mapContext, props.mapId]);
+    mapHook.map.setCenter([7.132122000552613, 50.716405378037706]);
+  }, [mapHook.map, props.mapId]);
 
   return <></>;
 };
