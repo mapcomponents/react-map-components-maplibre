aefa6ff59da13c65dff5b9d5a7c648e79368bcb2
Christian Kuntzsch <christian.kuntzsch@wheregroup.com>
2022-01-18 14:46:48 +0100

    Update markdown docs build script to incoporate docs in any language

--- scripts/build-catalogue-markdown-docs.js
+++ scripts/build-catalogue-markdown-docs.js
@@ -7,19 +7,21 @@ let options = {};
 function getComponentNameFromPath(path) {
   let tmp = path.split("/");
   tmp = tmp[tmp.length - 1];
-  return tmp.replace(".doc.de.md", "");
+  tmp = tmp.split(".doc");
+  return tmp[0];
 }
 
 const converter = new showdown.Converter();
 
-glob("src/components/**/*.doc.de.md", options, function (er, files) {
+glob("src/components/**/*.doc.*.md", options, function (er, files) {
   console.log(files);
 
   for (var i = 0, len = files.length; i < len; i++) {
+    let language = files[i].slice(-5, -3);
     let rawdata = fs.readFileSync(files[i]);
     let html = converter.makeHtml(rawdata + "");
     fs.writeFileSync(
-      "public/catalogue/" + getComponentNameFromPath(files[i]) + ".de.html",
+      "public/catalogue/" + getComponentNameFromPath(files[i]) + "." + language + ".html",
       html
     );
   }
