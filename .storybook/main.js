f806f21d26f74a7ed730e56615c3b87475972389
Max Tobias Weber <tobias.weber@wheregroup.com>
2022-04-20 11:49:14 +0200

    fix storybook stories.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
     {
       name: "@storybook/addon-docs",
       options: {
