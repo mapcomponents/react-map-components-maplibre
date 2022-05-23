dfaeaa37c431f9909b08d0d27f080b5f38eb4931
Nils Kaster <nils.kaster@wheregroup.com>
2022-05-23 10:54:42 +0200

    remove trivial useEffect hook

--- src/components/MlMeasureTool/MlMeasureTool.tsx
+++ src/components/MlMeasureTool/MlMeasureTool.tsx
@@ -1,43 +1,16 @@
-import React, {useRef, useEffect, useState} from "react";
-import useMap from "../../hooks/useMap";
+import React, { useState } from "react";
 import MlFeatureEditor from "../MlFeatureEditor/MlFeatureEditor";
 import * as turf from "@turf/turf";
 
 interface MlMeasureToolProps {
-  /**
-   * Id of the target MapLibre instance in mapContext
-   */
-  mapId?: string;
-  /**
-   * Id of an existing layer in the mapLibre instance to help specify the layer order
-   * This layer will be visually beneath the layer with the "insertBeforeLayer" id.
-   */
-  insertBeforeLayer?: string;
   /**
    * String that specify if the Tool measures an area ("polygon") or length ("line")
    */
   measureType: string;
 }
 
-/**
- * Tool that can measures either the length or area between dots created by mouse click events
- */
 const MlMeasureTool = (props: MlMeasureToolProps) => {
-  const mapHook = useMap({
-    mapId: props.mapId,
-    waitForLayer: props.insertBeforeLayer,
-  });
-  const initializedRef = useRef(false);
   const [length, setLength] = useState(0)
-
-  useEffect(() => {
-    if (!mapHook.map || initializedRef.current) return;
-    // the MapLibre-gl instance (mapHook.map) is accessible here
-    // initialize the layer and add it to the MapLibre-gl instance or do something else with it
-    initializedRef.current = true;
-
-    mapHook.map.map.setCenter([7.132122000552613, 50.716405378037706]);
-  }, [mapHook.map]);
 
   return (
     <>
