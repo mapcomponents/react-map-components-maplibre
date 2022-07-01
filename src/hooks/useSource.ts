fa7ed2abb884fbe51c054ef639e86484aea1281f
Fabian Polakowski <fabian.polakowski@wheregroup.com>
2022-07-01 18:43:00 +0200

    track if initiliazed and handle source as state variable

--- src/hooks/useSource.ts
+++ src/hooks/useSource.ts
@@ -24,7 +24,8 @@ function useSource(props: useSourceProps): useSourceType {
     mapId: props.mapId,
   });
 
-  const source = useRef<any>();
+  const initializedRef = useRef<boolean>(false);
+  const [source, setSource] = useState<any>();
   const sourceId = useRef(
     props.sourceId || (props.idPrefix ? props.idPrefix : "Source-") + mapHook.componentId
   );
@@ -36,6 +37,8 @@ function useSource(props: useSourceProps): useSourceType {
       mapHook.cleanup();
     }
 
+    initializedRef.current = true;
+
     mapHook.map?.addSource(
       props.sourceId,
       {
@@ -44,38 +47,40 @@ function useSource(props: useSourceProps): useSourceType {
       },
       mapHook.componentId
     );
-    mapHook.map?.addLayer(
-      {
-        id: props.sourceId,
-        type: "circle",
-        source: props.sourceId,
-        paint: {
-          "circle-radius": 6,
-          "circle-color": "#B42222",
-        },
-      },
-      false,
-      mapHook.componentId
-    );
-    source.current = mapHook.map.map.getSource(sourceId.current);
-    console.log(source.current);
+    //mapHook.map?.addLayer(
+    //  {
+    //    id: props.sourceId,
+    //    type: "circle",
+    //    source: props.sourceId,
+    //    paint: {
+    //      "circle-radius": 6,
+    //      "circle-color": "#B42222",
+    //    },
+    //  },
+    //  false,
+    //  mapHook.componentId
+    //);
+    setSource(mapHook.map.map.getSource(sourceId.current));
+    console.log(source);
   }, [props, mapHook.map]);
 
   // cleanup
   useEffect(() => {
     return () => {
+      initializedRef.current = false;
       mapHook.cleanup();
     };
   }, []);
 
   useEffect(() => {
-    if (!mapHook.map) return;
+    if (!mapHook.map || initializedRef.current) return;
+
     createSource();
   }, [mapHook.map, createSource]);
 
   return {
     map: mapHook.map,
-    source: source.current,
+    source: source,
     sourceId: sourceId.current,
     componentId: mapHook.componentId,
     mapHook: mapHook,
