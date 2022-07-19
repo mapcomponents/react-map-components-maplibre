550aa544187f610cf5b8bfc48a926adf0fe3e7fc
Max Tobias Weber <tobias.weber@wheregroup.com>
2022-07-19 12:45:28 +0200

    add more configuration options to MlNavigationTools

--- src/components/MlNavigationTools/MlNavigationTools.stories.js
+++ src/components/MlNavigationTools/MlNavigationTools.stories.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import MlNavigationTools from "./MlNavigationTools";
 
-import mapContextDecorator from "../../decorators/MapContextDecorator";
+import noNavToolsDecorator from "../../decorators/NoNavToolsDecorator";
+import BuildIcon from "@mui/icons-material/Build";
+import Button from "@mui/material/Button";
 
 const storyoptions = {
   title: "MapComponents/MlNavigationTools",
@@ -11,13 +13,47 @@ const storyoptions = {
     url: {},
     layer: {},
   },
-  decorators: mapContextDecorator,
+  decorators: noNavToolsDecorator,
 };
 export default storyoptions;
 
 const Template = (args) => <MlNavigationTools {...args} />;
 
-export const ExampleConfig = Template.bind({});
-ExampleConfig.parameters = {};
-ExampleConfig.args = {
+export const DefaultConfig = Template.bind({});
+DefaultConfig.parameters = {};
+DefaultConfig.args = {};
+
+export const No3DButton = Template.bind({});
+No3DButton.parameters = {};
+No3DButton.args = {
+  show3DButton: false,
+};
+
+export const ShowCenterLocationButton = Template.bind({});
+ShowCenterLocationButton.parameters = {};
+ShowCenterLocationButton.args = {
+  showFollowGpsButton: false,
+  showCenterLocationButton: true,
+};
+
+export const NoZoomButtons = Template.bind({});
+NoZoomButtons.parameters = {};
+NoZoomButtons.args = {
+  showZoomButtons: false,
+};
+
+export const NoFollowGpsButton = Template.bind({});
+NoFollowGpsButton.parameters = {};
+NoFollowGpsButton.args = {
+  showFollowGpsButton: false,
+};
+
+export const CustomButton = Template.bind({});
+CustomButton.parameters = {};
+CustomButton.args = {
+  children: (
+    <Button onClick={() => {}}>
+      <BuildIcon sx={{ fontSize: "1em" }} />
+    </Button>
+  ),
 };
