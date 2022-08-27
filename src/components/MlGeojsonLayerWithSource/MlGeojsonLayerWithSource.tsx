6d77672c2dfc88f48ab173e6bc97129e7eff5a47
Fabian Polakowski <fabian.polakowski@wheregroup.com>
2022-08-27 18:03:10 +0200

    add source and layer

--- src/components/MlGeojsonLayerWithSource/MlGeojsonLayerWithSource.tsx
+++ src/components/MlGeojsonLayerWithSource/MlGeojsonLayerWithSource.tsx
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import useMap from "../../hooks/useMap";
+import useLayer from "../../hooks/useLayer";
 import useSource from "../../hooks/useSource";
-import wg_geojson from "./assets/pointWG.json";
 
 interface MlGeojsonLayerWithSourceProps {
   /**
@@ -20,19 +20,64 @@ interface MlGeojsonLayerWithSourceProps {
  *
  */
 const MlGeojsonLayerWithSource = (props: MlGeojsonLayerWithSourceProps) => {
+  const wg_geojson = {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [7.0851268, 50.73884],
+      properties: {
+        title: "WhereGroup GmbH",
+      },
+    },
+  };
+
   const mapHook = useMap({
     mapId: props.mapId,
     waitForLayer: props.insertBeforeLayer,
   });
+
   const initializedRef = useRef(false);
 
-  const { source } = useSource({
+  const sourceId = "geojson-source";
+  useSource({
     mapId: props.mapId,
-    sourceId: "geojson-source",
+    sourceId: sourceId,
     source: {
       type: "geojson",
       data: wg_geojson,
     },
+  });
+
+  useLayer({
+    mapId: props.mapId,
+    layerId: "layer1",
+    source: sourceId,
+    options: {
+      id: "layer1",
+      source: sourceId,
+      type: "circle",
+      paint: {
+        "circle-radius": 6,
+        "circle-color": "red",
+      },
+    },
+    insertBeforeLayer: false,
+  });
+
+  useLayer({
+    mapId: props.mapId,
+    layerId: "layer2",
+    source: sourceId,
+    options: {
+      id: "layer2",
+      source: sourceId,
+      type: "circle",
+      paint: {
+        "circle-radius": 4,
+        "circle-color": "green",
+      },
+    },
+    insertBeforeLayer: false,
   });
 
   useEffect(() => {
@@ -41,7 +86,7 @@ const MlGeojsonLayerWithSource = (props: MlGeojsonLayerWithSourceProps) => {
     // initialize the layer and add it to the MapLibre-gl instance or do something else with it
     initializedRef.current = true;
 
-    mapHook.map.map.setCenter([7.132122000552613, 50.716405378037706]);
+    mapHook.map.map.setCenter([7.0851268, 50.73884]);
   }, [mapHook.map, props.mapId]);
 
   return <></>;
