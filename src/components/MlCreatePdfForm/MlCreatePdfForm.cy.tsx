f6af0fde96f30385698fc04104c2fd71947de1d9
Max Tobias Weber <tobias.weber@wheregroup.com>
2022-11-28 09:50:27 +0100

    Feature/cypress setup (#48)
    * delete unsused cra files
    
    * cleanup storybook config, remove cra references
    
    * add cypress test setup
    
    * fix eslint warnings

---
+++ src/components/MlCreatePdfForm/MlCreatePdfForm.cy.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { composeStories } from '@storybook/testing-react';
+import { mount } from '@cypress/react';
+import * as stories from "./MlCreatePdfForm.stories";
+
+// compile the "Primary" story with the library
+const {ExampleConfig } = composeStories(stories);
+
+describe('MlCreatePdfForm Tests', () => {
+	it('Should generate and download a PDF export of the current map preview', () => {
+		// and mount the story using @cypress/react library
+		mount(<ExampleConfig />);
+
+    cy.get(".pdfFormButton").click();
+ 
+		cy.wait(1000)
+    cy.get(".createPdfButton").click();
+		cy.wait(2000)
+    cy.readFile("./cypress/downloads/Map.pdf").should('contain', 'WhereGroup')
+	});
+});
--- src/components/MlCreatePdfForm/MlCreatePdfForm.stories.tsx
+++ src/components/MlCreatePdfForm/MlCreatePdfForm.stories.tsx
@@ -51,14 +51,12 @@ export default storyoptions;
 
 const Template = () => {
 	const [showCreatePdfForm, setShowCreatePdfForm] = useState(false);
-	const targetRef = React.useRef<HTMLDivElement>(null);
-	const containerRef = React.useRef<HTMLDivElement>(document.querySelector('.map_container'));
 
 	// <MlWmsLayer url='https://geo.stat.fi/geoserver/vaestoruutu/wms' urlParameters={{layers:'vaki2005_1km_kp'}}/>
 	return (
 		<>
 			<TopToolbar>
-				<Button variant="contained" onClick={() => setShowCreatePdfForm(!showCreatePdfForm)}>
+				<Button variant="contained" className='pdfFormButton' onClick={() => setShowCreatePdfForm(!showCreatePdfForm)}>
 					PDF
 				</Button>
 			</TopToolbar>
