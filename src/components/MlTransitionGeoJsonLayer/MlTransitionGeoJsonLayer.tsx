ae0aa98c3013f8a1acbd38a6039a4dd6f784071c
Fabian Polakowski <f.polakowski@gmx.de>
2022-12-29 11:05:49 +0100

    Feature/additional geojson layertypes (#57)
    * support all 'type' types for geojson layer and add heatmap example
    
    * add layout and paint  types and use them in the heatmap example
    
    * Add Circle Example to MlGeoJsonLayer
    
    * Exclude Raster
    
    * Fix type. It must be matching with new MlGeoJsonLayerType

--- src/components/MlTransitionGeoJsonLayer/MlTransitionGeoJsonLayer.tsx
+++ src/components/MlTransitionGeoJsonLayer/MlTransitionGeoJsonLayer.tsx
@@ -1,83 +1,23 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-
-import * as turf from "@turf/turf";
-
-import useMap from "../../hooks/useMap";
-
-import { _transitionToGeojson } from "./util/transitionFunctions";
-import MlGeoJsonLayer from "../MlGeoJsonLayer/MlGeoJsonLayer";
-import { Feature, FeatureCollection } from "@turf/turf";
-import { CircleLayerSpecification, FillLayerSpecification, LineLayerSpecification, MapLayerMouseEvent } from "maplibre-gl";
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import * as turf from '@turf/turf';
+import useMap from '../../hooks/useMap';
+import { _transitionToGeojson } from './util/transitionFunctions';
+import MlGeoJsonLayer from '../MlGeoJsonLayer/MlGeoJsonLayer';
+import { Feature, FeatureCollection } from '@turf/turf';
+import { MlGeoJsonLayerProps } from '../MlGeoJsonLayer/MlGeoJsonLayer';
 
 const msPerStep = 50;
 
-interface MlTransitionGeoJsonLayerProps {
-	/**
-	 * Id of the target MapLibre instance in mapContext
-	 */
-	mapId: string;
-	/**
-	 * GeoJSON data that is supposed to be rendered by this component.
-	 */
-	geojson: Feature | FeatureCollection | undefined;
-	/**
-	 * Type of the layer that will be added to the MapLibre instance.
-	 * Possible values: "line", "circle", "fill"
-	 */
-	type?: "fill" | "line" | "circle";
-	/**
-	 * Paint property object, that is passed to the addLayer call.
-	 * Possible props depend on the layer type.
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#line
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#circle
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#fill
-	 */
-	paint?: CircleLayerSpecification['paint'] | FillLayerSpecification['paint'] | LineLayerSpecification['paint'];
-	/**
-	 * Layout property object, that is passed to the addLayer call.
-	 * Possible props depend on the layer type.
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#line
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#circle
-	 * https://maplibre.org/maplibre-gl-js-docs/style-spec/layers/#fill
-	 */
-	layout?: CircleLayerSpecification['layout'] | FillLayerSpecification['layout'] | LineLayerSpecification['layout'];
-	/**
-	 * Javascript object that is spread into the addLayer commands first parameter.
-	 */
-	options?: CircleLayerSpecification | FillLayerSpecification | LineLayerSpecification;
-	/**
-	 * Javascript object with optional properties "fill", "line", "circle" to override implicit layer type default paint properties.
-	 */
-	defaultPaintOverrides?: { circle?: CircleLayerSpecification['paint'], fill?: FillLayerSpecification['paint'], line?: LineLayerSpecification['paint'] };
-	/**
-	 * Id of an existing layer in the mapLibre instance to help specify the layer order
-	 * This layer will be visually beneath the layer with the "insertBeforeLayer" id.
-	 */
-	insertBeforeLayer: string;
-	/**
-	 * Hover event handler that is executed whenever a geometry rendered by this component is hovered.
-	 */
-	onHover?: MapLayerMouseEvent;
-	/**
-	 * Click event handler that is executed whenever a geometry rendered by this component is clicked.
-	 */
-	onClick?: MapLayerMouseEvent;
-	/**
-	 * Leave event handler that is executed whenever a geometry rendered by this component is
-	 * left/unhovered.
-	 */
-	onLeave?: MapLayerMouseEvent;
-	/**
-	 * Creates transition animation whenever the geojson prop changes.
-	 * Only works with layer type "line" and LineString GeoJSON data.
-	 */
+type MlTransitionGeoJsonLayerProps = MlGeoJsonLayerProps & {
 	transitionTime: number;
-}
+};
 
 /**
  * Adds source and layer of types "line", "fill" or "circle" to display GeoJSON data on the map.
  */
 const MlTransitionGeoJsonLayer = (props: MlTransitionGeoJsonLayerProps) => {
+	// ignore eslint. Only using `geojson` for destructuring
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { geojson, ...restProps } = props;
 
 	const mapHook = useMap({
@@ -93,9 +33,7 @@ const MlTransitionGeoJsonLayer = (props: MlTransitionGeoJsonLayerProps) => {
 	const currentTransitionStepRef = useRef(false);
 	const transitionGeojsonDataRef = useRef([]);
 	const transitionGeojsonCommonDataRef = useRef([]);
-	const [displayGeojson, setDisplayGeojson] = useState(
-		turf.featureCollection([])
-	);
+	const [displayGeojson, setDisplayGeojson] = useState(turf.featureCollection([]));
 
 	useEffect(() => {
 		return () => {
@@ -125,8 +63,8 @@ const MlTransitionGeoJsonLayer = (props: MlTransitionGeoJsonLayerProps) => {
 		if (!mapHook.map || !initializedRef.current) return;
 
 		if (
-			typeof props.transitionTime !== "undefined" &&
-			props.type === "line" &&
+			typeof props.transitionTime !== 'undefined' &&
+			props.type === 'line' &&
 			oldGeojsonRef.current
 		) {
 			transitionInProgressRef.current = false;
@@ -140,10 +78,10 @@ const MlTransitionGeoJsonLayer = (props: MlTransitionGeoJsonLayerProps) => {
 
 	const startTransition = useCallback(() => {
 		if (
-			props.type === "line" &&
-			typeof props.transitionTime !== "undefined" &&
+			props.type === 'line' &&
+			typeof props.transitionTime !== 'undefined' &&
 			props.transitionTime &&
-			typeof props.geojson !== "undefined" &&
+			typeof props.geojson !== 'undefined' &&
 			JSON.stringify(oldGeojsonRef.current) !== JSON.stringify(props.geojson)
 		) {
 			transitionToGeojson();
