8f3a5678b1b71a207d069eec8e0d630dc13a5c11
Martin Alzueta <martin.alzueta@wheregroup.com>
2023-01-09 11:46:37 +0100

    new instructions for GPXViewer

--- src/components/MlGpxViewer/util/MlGpxViewerInstructions.tsx
+++ src/components/MlGpxViewer/util/MlGpxViewerInstructions.tsx
@@ -1,89 +1,99 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Fade from '@mui/material/Fade';
 import BubbleStyle from '../../../util/BubbleForInstructions';
 
-function BubbleBottomRight(props: { iconMarginTop: string; children: unknown }) {
-	return (
-		<BubbleStyle
-			bubbleRight="-120px"
-			bubbleBottom="-120px"
-			textMarginTop="70px"
-			textMarginLeft="120px"
-			iconTransform="rotate(180deg)"
-			iconMarginTop={props.iconMarginTop}
-			iconMarginLeft="200px"
-		>
-			{props.children}
-		</BubbleStyle>
-	);
-}
+const bubbleBottomRightStyle = {
+	bubbleRight: '-120px',
+	bubbleBottom: '-120px',
+	textMarginTop: '70px',
+	textMarginLeft: '120px',
+	iconTransform: 'rotate(180deg)',
+	iconMarginLeft: '200px',
+};
 
 export default function MlGpxViewerInstructions(props: { open: boolean; callback: () => void }) {
-	const [step1, setStep1] = useState(false);
-	const [step2, setStep2] = useState(false);
-	const [step4, setStep4] = useState(false);
+	const [activeStep, setActiveStep] = useState<number>();
+	const initializedRef = useRef(false);
+	const steps = [
+		{
+			duration: 3000,
+			props: {
+				...bubbleBottomRightStyle,
+				iconMarginTop: '43px',
+			},
+			content: (
+				<>
+					You can load your <br />
+					own GPX file <br /> here
+				</>
+			),
+		},
+		{
+			duration: 3000,
+			props: {
+				...bubbleBottomRightStyle,
+				iconMarginTop: '68px',
+			},
+			content: (
+				<>
+					You can see more <br /> information about <br />
+					the track <br />
+					here
+				</>
+			),
+		},
+		{
+			duration: 3000,
+			props: {
+				bubbleLeft: '-40px',
+				bubbleTop: '-150px',
+				textMarginTop: '230px',
+				textMarginLeft: '75px',
+				iconTransform: 'rotate(90deg)',
+				iconMarginTop: '-135px',
+				iconMarginLeft: '280px',
+			},
+			content: (
+				<>
+					You can load your <br />
+					own GPX file <br /> here
+				</>
+			),
+		},
+	];
+
+	const activateStep = (stepId?: number | undefined) => {
+			let _nextStep: number | undefined = typeof stepId === 'undefined' ? 0 : stepId + 1;
+			if (typeof _nextStep !== 'undefined') {
+				if (_nextStep > steps.length + 1) {
+					_nextStep = undefined;
+				} else {
+					setTimeout(() => {
+						activateStep(_nextStep);
+					}, steps[_nextStep].duration);
+				}
+			}
+			setActiveStep(_nextStep);
+		}
+	;
 
 	useEffect(() => {
-		if (props.open) {
-			setStep1(true);
-
-			setTimeout(() => {
-				setStep1(false);
-				setStep2(true);
-			}, 3000);
-			setTimeout(() => {
-				setStep2(false);
-				setStep4(true);
-			}, 6000);
-			setTimeout(() => {
-				setStep4(false);
-				if (typeof props.callback === 'function') {
-					props.callback();
-				}
-			}, 9000);
-		} else {
-			return;
+		if (props.open && !initializedRef.current) {
+			initializedRef.current = true;
+			activateStep();
+		}
+		if (!props.open) {
+			initializedRef.current = false;
+			setActiveStep(undefined);
 		}
 	}, [props.open]);
 
 	return (
 		<>
-			{step1 && (
-				<Fade in={step1} timeout={150}>
+			{typeof activeStep !== 'undefined' && (
+				<Fade in={true} timeout={150}>
 					<div>
-						<BubbleBottomRight iconMarginTop="43px">
-							You can load your <br />
-							own GPX file <br /> here
-						</BubbleBottomRight>
-					</div>
-				</Fade>
-			)}
-			{step2 && (
-				<Fade in={step2} timeout={150}>
-					<div>
-						<BubbleBottomRight iconMarginTop="68px">
-							You can see more <br /> information about <br />
-							the track <br />
-							here
-						</BubbleBottomRight>
-					</div>
-				</Fade>
-			)}
-
-			{step4 && (
-				<Fade in={step4} timeout={1500}>
-					<div>
-						<BubbleStyle
-							bubbleLeft="-40px"
-							bubbleTop="-150px"
-							textMarginTop="230px"
-							textMarginLeft="75px"
-							iconTransform="rotate(90deg)"
-							iconMarginTop="-135px"
-							iconMarginLeft="280px"
-						>
-							In demo mode we <br /> provide you some <br /> GPX tracks <br /> to load.
-						</BubbleStyle>
+						<BubbleStyle {...steps[activeStep].props}>{steps[activeStep].content}</BubbleStyle>
 					</div>
 				</Fade>
 			)}
