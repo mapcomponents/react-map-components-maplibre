d3482f4d25af8df5ebd6542b62586560b65c1b96
Max Tobias Weber <tobias.weber@wheregroup.com>
2023-01-15 17:46:06 +0100

    Make WmsLayer urlParameters attribute dynamic; improve WmwsLoader performance by making it load all layers using a single WmsLayer component; add attribution functionality to wmsLoader

--- src/components/MlWmsLoader/MlWmsLoader.tsx
+++ src/components/MlWmsLoader/MlWmsLoader.tsx
@@ -1,22 +1,21 @@
-import React, { useRef, useEffect, useContext, useCallback, useState } from "react";
+import React, { useRef, useEffect, useContext, useCallback, useState, useMemo } from 'react';
 
-import MapContext from "../../contexts/MapContext";
-import { v4 as uuidv4 } from "uuid";
+import MapContext from '../../contexts/MapContext';
+import { v4 as uuidv4 } from 'uuid';
 
-import MlWmsLayer from "../MlWmsLayer/MlWmsLayer";
-import MlMarker from "../MlMarker/MlMarker";
-import MlLayer from "../MlLayer/MlLayer";
-import useWms, { useWmsProps } from "../../hooks/useWms";
+import MlWmsLayer from '../MlWmsLayer/MlWmsLayer';
+import MlMarker from '../MlMarker/MlMarker';
+import useWms, { useWmsProps } from '../../hooks/useWms';
 
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import IconButton from "@mui/material/IconButton";
-import { LngLat } from "maplibre-gl";
-import MapLibreGlWrapper from "../MapLibreMap/lib/MapLibreGlWrapper";
-import { Layer2 } from "wms-capabilities";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import { LngLat } from 'maplibre-gl';
+import MapLibreGlWrapper from '../MapLibreMap/lib/MapLibreGlWrapper';
+import { Layer2, Layer3 } from 'wms-capabilities';
 import { useWmsReturnType } from '../../hooks/useWms';
 
 const originShift = (2 * Math.PI * 6378137) / 2.0;
@@ -49,15 +48,17 @@ export interface MlWmsLoaderProps {
 	/**
 	 * URL parameters that will be added when requesting WMS capabilities
 	 */
-	wmsUrlParameters?: {[key: string]: string};
+	wmsUrlParameters?: { [key: string]: string };
 	lngLat?: LngLat;
 	idPrefix?: string;
 }
 
-	export type LayerType = {
-		visible: boolean;
-		Name: string;
-	} & Layer2;
+export type LayerType = {
+	visible: boolean;
+	Name: string;
+	Attribution?: { Title: string };
+} & Omit<Layer2, 'Layer'> &
+	Partial<Pick<Layer2, 'Layer'>>;
 /**
  * Loads a WMS getCapabilities xml document and adds a MlWmsLayer component for each layer that is
  * offered by the WMS.
@@ -69,16 +70,18 @@ export interface MlWmsLoaderProps {
 const MlWmsLoader = (props: MlWmsLoaderProps) => {
 	// Use a useRef hook to reference the layer object to be able to access it later inside useEffect hooks
 	const mapContext: MapContextType = useContext(MapContext);
-	const { capabilities, error, setUrl, getFeatureInfoUrl, wmsUrl }:useWmsReturnType = useWms({
+	const { capabilities, error, setUrl, getFeatureInfoUrl, wmsUrl }: useWmsReturnType = useWms({
 		urlParameters: props.urlParameters,
 	});
 
 	const initializedRef = useRef(false);
 	const mapRef = useRef<MapLibreGlWrapper>();
-	const componentId = useRef((props.idPrefix ? props.idPrefix : "MlWmsLoader-") + uuidv4());
+	const componentId = useRef((props.idPrefix ? props.idPrefix : 'MlWmsLoader-') + uuidv4());
 	const [layers, setLayers] = useState<Array<LayerType>>([]);
 
-	const [featureInfoLngLat, setFeatureInfoLngLat] = useState<{lng:number,lat:number} | undefined>();
+	const [featureInfoLngLat, setFeatureInfoLngLat] = useState<
+		{ lng: number; lat: number } | undefined
+	>();
 	const [featureInfoContent, setFeatureInfoContent] = useState<string | undefined>(undefined);
 
 	useEffect(() => {
@@ -105,6 +108,14 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 		setUrl(props.url);
 	}, [props.url]);
 
+	const attribution = useMemo(() => {
+		return layers
+			.filter((el) => el.visible && el?.Attribution?.Title)
+			.map((el) => el?.Attribution?.Title)
+			.filter((value, index, self) => self.indexOf(value) === index)
+			.join(' ');
+	}, [layers]);
+
 	const getFeatureInfo = useCallback(
 		(ev) => {
 			if (!mapRef.current) return;
@@ -115,41 +126,37 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 			const _ne = lngLatToMeters(_bounds._ne);
 			const bbox = [_sw[0], _sw[1], _ne[0], _ne[1]];
 			const _getFeatureInfoUrlParams = {
-				REQUEST: "GetFeatureInfo",
+				REQUEST: 'GetFeatureInfo',
 
-				BBOX: bbox.join(","),
-				SERVICE: "WMS",
+				BBOX: bbox.join(','),
+				SERVICE: 'WMS',
 				INFO_FORMAT:
-					capabilities?.Capability?.Request?.GetFeatureInfo.Format.indexOf("text/html") !== -1
-						? "text/html"
-						: "text/plain",
-				FEATURE_COUNT: "10",
+					capabilities?.Capability?.Request?.GetFeatureInfo.Format.indexOf('text/html') !== -1
+						? 'text/html'
+						: 'text/plain',
+				FEATURE_COUNT: '10',
 				LAYERS: layers
-					.map((layer: LayerType ) =>
-						layer.visible && layer.queryable ? layer.Title : undefined
-					)
+					.map((layer: LayerType) => (layer.visible && layer.queryable ? layer.Title : undefined))
 					.filter((n) => n),
 				QUERY_LAYERS: layers
-					.map((layer: LayerType) =>
-						layer.visible && layer.queryable ? layer.Title : undefined
-					)
+					.map((layer: LayerType) => (layer.visible && layer.queryable ? layer.Title : undefined))
 					.filter((n) => n),
 				WIDTH: mapRef.current?.map._container.clientWidth,
 				HEIGHT: mapRef.current?.map._container.clientHeight,
-				srs: "EPSG:3857",
-				CRS: "EPSG:3857",
-				version: "1.3.0",
+				srs: 'EPSG:3857',
+				CRS: 'EPSG:3857',
+				version: '1.3.0',
 				X: ev.point.x,
 				Y: ev.point.y,
 				I: ev.point.x,
 				J: ev.point.y,
-				buffer: "50",
+				buffer: '50',
 			};
 
 			let _gfiUrl: string | undefined = getFeatureInfoUrl;
 			let _gfiUrlParts;
-			if (_gfiUrl?.indexOf?.("?") !== -1) {
-				_gfiUrlParts = props.url.split("?");
+			if (_gfiUrl?.indexOf?.('?') !== -1) {
+				_gfiUrlParts = props.url.split('?');
 				_gfiUrl = _gfiUrlParts[0];
 			}
 			const _urlParamsFromUrl = new URLSearchParams(_gfiUrlParts?.[1]);
@@ -162,10 +169,10 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 			// "as any" can be removed once the URLSearchParams ts spec is fixed
 			const urlParams = new URLSearchParams(urlParamsObj as unknown as Record<string, string>);
 
-			fetch(props.url + "?" + urlParams.toString())
+			fetch(props.url + '?' + urlParams.toString())
 				.then((res) => {
 					if (!res.ok) {
-						throw new Error("FeatureInfo could not be fetched");
+						throw new Error('FeatureInfo could not be fetched');
 					}
 					return res.text();
 				})
@@ -183,36 +190,61 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 
 		const _getFeatureInfo = getFeatureInfo;
 
-		mapRef.current.on("click", _getFeatureInfo, componentId.current);
+		mapRef.current.on('click', _getFeatureInfo, componentId.current);
 		return () => {
-			mapRef.current?.map.off?.("click", _getFeatureInfo);
+			mapRef.current?.map.off?.('click', _getFeatureInfo);
 		};
 	}, [getFeatureInfo]);
 
 	useEffect(() => {
 		if (!capabilities?.Service) return;
 
-		if (capabilities?.Capability?.Layer?.CRS?.indexOf?.("EPSG:3857") === -1) {
+		if (capabilities?.Capability?.Layer?.CRS?.indexOf?.('EPSG:3857') === -1) {
 			console.log(
-				"MlWmsLoader (" + capabilities.Service.Title + "): No WGS 84/Pseudo-Mercator support"
+				'MlWmsLoader (' + capabilities.Service.Title + '): No WGS 84/Pseudo-Mercator support'
 			);
 		} else {
 			console.log(
-				"MlWmsLoader (" + capabilities.Service.Title + "): WGS 84/Pseudo-Mercator supported"
+				'MlWmsLoader (' + capabilities.Service.Title + '): WGS 84/Pseudo-Mercator supported'
 			);
 
 			let _LatLonBoundingBox: Array<number> = [];
-			setLayers(
-				capabilities?.Capability?.Layer?.Layer.map((layer: Layer2 & {Name:string}, idx: number) => {
-					if (idx === 0) {
-						_LatLonBoundingBox = layer.EX_GeographicBoundingBox;
+
+			// collect aueriable Layer2 layers
+			let _layers: LayerType[] = capabilities?.Capability?.Layer?.Layer.filter(
+				(el) => el.queryable
+			).map((layer: Layer2 & { Name: string }, idx: number) => {
+				if (idx === 0) {
+					_LatLonBoundingBox = layer.EX_GeographicBoundingBox;
+				}
+				return {
+					visible: capabilities?.Capability?.Layer?.Layer?.length > 2 ? idx > 1 : true,
+					Attribution: { Title: '' },
+					...layer,
+				};
+			});
+
+			// collect aueriable Layer3 layers
+			capabilities?.Capability?.Layer?.Layer.forEach((el) => {
+				const tmpLayers = el?.Layer?.filter((el) => el.queryable).map(
+					(layer: Layer3, idx: number) => {
+						if (idx === 0) {
+							_LatLonBoundingBox = layer.EX_GeographicBoundingBox;
+						}
+						return {
+							visible: false,
+							Attribution: { Title: '' },
+							...layer,
+						};
 					}
-					return {
-						visible: capabilities?.Capability?.Layer?.Layer?.length > 2 ? idx > 1 : true,
-						...layer
-					};
-				})
-			);
+				);
+
+				if (tmpLayers) {
+					_layers = [..._layers, ...tmpLayers];
+				}
+			});
+
+			setLayers(_layers);
 
 			// zoom to extent of first layer
 			if (mapRef.current && _LatLonBoundingBox.length > 3) {
@@ -230,7 +262,6 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 		// initialize the layer and add it to the MapLibre-gl instance or do something else with it
 		initializedRef.current = true;
 		mapRef.current = mapContext.getMap(props.mapId);
-		console.log("set url " + props.url);
 		setUrl(props.url);
 	}, [mapContext.mapIds, mapContext, props.mapId, props.url]);
 
@@ -238,23 +269,9 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 		<>
 			{error && <p>{error}</p>}
 			<h3 key="title">{capabilities?.Service?.Title}</h3>
-			{console.log(componentId.current)}
-			{capabilities?.Capability?.Layer?.Layer.map((_layer: LayerType, idx: number) => (
-				<MlLayer
-					layerId={"Order-" + componentId.current + "-" + idx}
-					key={componentId.current + "-" + idx}
-					{...(idx > 0
-						? {
-								insertBeforeLayer: "Order-" + componentId.current + "-" + (idx - 1),
-							}
-						: undefined)}
-				/>
-			))}
 			<List dense key="layers">
 				{wmsUrl &&
 					layers?.map?.((layer, idx) => {
-						console.log(layer);
-						
 						return layer?.Name ? (
 							<ListItem
 								key={layer.Name + idx}
@@ -272,21 +289,29 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 									</IconButton>
 								}
 							>
-								<ListItemText primary={layer?.Title} secondary={layer?.Abstract} />
-								<MlWmsLayer
-									key={layer?.Name + idx}
-									url={wmsUrl}
-									urlParameters={{ ...props.wmsUrlParameters, layers: layer?.Name }}
-									visible={layers[idx].visible}
-									insertBeforeLayer={"Order-" + componentId.current + "-" + idx}
-								/>
+								<ListItemText primary={layer?.Title} />
 							</ListItem>
 						) : (
 							<></>
 						);
 					})}
 			</List>
-			<p key="description" style={{ fontSize: ".7em" }}>
+			{wmsUrl && layers?.length && (
+				<MlWmsLayer
+					key={componentId.current}
+					url={wmsUrl}
+					attribution={attribution}
+					urlParameters={{
+						...props.wmsUrlParameters,
+						layers: layers
+							?.filter?.((layer) => layer.visible)
+							.map((el) => el.Name)
+							.join(','),
+					}}
+				/>
+			)}
+
+			<p key="description" style={{ fontSize: '.7em' }}>
 				{capabilities?.Capability?.Layer?.['Abstract']}
 			</p>
 
@@ -296,14 +321,14 @@ const MlWmsLoader = (props: MlWmsLoaderProps) => {
 };
 
 MlWmsLoader.defaultProps = {
-	url: "",
+	url: '',
 	urlParameters: {
-		SERVICE: "WMS",
-		VERSION: "1.3.0",
-		REQUEST: "GetCapabilities",
+		SERVICE: 'WMS',
+		VERSION: '1.3.0',
+		REQUEST: 'GetCapabilities',
 	},
 	wmsUrlParameters: {
-		TRANSPARENT: "TRUE",
+		TRANSPARENT: 'TRUE',
 	},
 };
 
