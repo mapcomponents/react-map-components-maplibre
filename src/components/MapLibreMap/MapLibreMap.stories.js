943c3ccc720c6ae8a06fd3917a92196a53ca649a
mludwig <marwin.ludwig@wheregroup.com>
2023-02-01 10:48:56 +0100

    add mapContextDecorator to MapLibreMap Storie

--- src/components/MapLibreMap/MapLibreMap.stories.js
+++ src/components/MapLibreMap/MapLibreMap.stories.js
@@ -7,47 +7,47 @@ import { Button } from "@mui/material";
 import TopToolbar from "../../ui_components/TopToolbar";
 import sample_geojson_1 from "../MlGeoJsonLayer/assets/sample_1.json";
 
+import mapContextDecorator from '../../decorators/MapContextDecorator';
+
 const storyoptions = {
-	title: "Core/MapLibreMap",
+	title: 'Core/MapLibreMap',
 	component: MapLibreMap,
 	argTypes: {
 		options: {
 			control: {
-				type: "object",
+				type: 'object',
 			},
 		},
 	},
-
-  parameters: {
-    sourceLink: 'components/MapLibreMap/MapLibreMap.tsx'
-	}
+	decorators: mapContextDecorator,
+	parameters: {
+		sourceLink: 'components/MapLibreMap/MapLibreMap.tsx',
+	},
 };
 export default storyoptions;
 
 const Template = (args) => {
 	return (
-		<MapComponentsProvider>
-			<MapLibreMap
-				options={{ ...args.options }}
-				style={{
-					position: "absolute",
-					height: "100vh",
-					width: "100vw",
-					top: 0,
-					right: 0,
-					left: 0,
-					bottom: 0,
-					zIndex: 100,
-				}}
-			/>
-		</MapComponentsProvider>
+		<MapLibreMap
+			options={{ ...args.options }}
+			style={{
+				position: 'absolute',
+				height: '100vh',
+				width: '100vw',
+				top: 0,
+				right: 0,
+				left: 0,
+				bottom: 0,
+				zIndex: 100,
+			}}
+		/>
 	);
 };
 
 export const ExampleConfig = Template.bind({});
 ExampleConfig.args = {
 	options: {
-		style: "https://wms.wheregroup.com/tileserver/style/osm-bright.json",
+		style: 'https://wms.wheregroup.com/tileserver/style/osm-bright.json',
 		center: [8.607, 53.1409349],
 		zoom: 14,
 	},
@@ -55,24 +55,24 @@ ExampleConfig.args = {
 
 const StyleChangeTemplate = (args) => {
 	const [activeStyle, setActiveStyle] = useState(
-		"https://wms.wheregroup.com/tileserver/style/osm-fiord-color.json"
+		'https://wms.wheregroup.com/tileserver/style/osm-fiord-color.json'
 	);
 
 	return (
-		<MapComponentsProvider>
+		<>
 			<TopToolbar>
 				<Button
 					onClick={() =>
-						setActiveStyle("https://wms.wheregroup.com/tileserver/style/osm-bright.json")
+						setActiveStyle('https://wms.wheregroup.com/tileserver/style/osm-bright.json')
 					}
 					variant="contained"
-					sx={{marginRight:'5px'}}
+					sx={{ marginRight: '5px' }}
 				>
 					OSM-Bright
 				</Button>
 				<Button
 					onClick={() =>
-						setActiveStyle("https://wms.wheregroup.com/tileserver/style/osm-fiord-color.json")
+						setActiveStyle('https://wms.wheregroup.com/tileserver/style/osm-fiord-color.json')
 					}
 					variant="contained"
 				>
@@ -82,9 +82,9 @@ const StyleChangeTemplate = (args) => {
 			<MapLibreMap
 				options={{ ...args.options, style: activeStyle }}
 				style={{
-					position: "absolute",
-					height: "100vh",
-					width: "100vw",
+					position: 'absolute',
+					height: '100vh',
+					width: '100vw',
 					top: 0,
 					right: 0,
 					left: 0,
@@ -93,7 +93,7 @@ const StyleChangeTemplate = (args) => {
 				}}
 			/>
 			<MlGeoJsonLayer type="line" geojson={sample_geojson_1} />
-		</MapComponentsProvider>
+		</>
 	);
 };
 
