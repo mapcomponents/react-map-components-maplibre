0303b4a2333653cf1bb7666a4bb1cc8446124b5d
mludwig <marwin.ludwig@wheregroup.com>
2023-02-07 14:46:29 +0100

    remove unused

--- src/ui_components/TopToolbar.tsx
+++ src/ui_components/TopToolbar.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
