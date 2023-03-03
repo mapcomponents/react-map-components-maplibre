f3f4a96c629aa60411f8db81ec0af6d73371311d
Max Tobias Weber <tobias.weber@wheregroup.com>
2023-03-03 13:40:47 +0100

    remove unused import from TopToolbar

--- src/ui_components/TopToolbar.tsx
+++ src/ui_components/TopToolbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
