2f9cbfec6e611e1f298efede5419026fd2d22569
Max Tobias Weber <maxtobiasweber@gmail.com>
2023-03-06 11:51:20 +0100

    add disabled TextField to ColorPicker

--- src/ui_components/ColorPicker/ColorPicker.tsx
+++ src/ui_components/ColorPicker/ColorPicker.tsx
@@ -1,31 +1,35 @@
 import React, { useState } from 'react';
 
-import { ChromePicker, } from 'react-color';
-import { Button } from '@mui/material';
+import { ChromePicker } from 'react-color';
+import { Button, Grid, TextField } from '@mui/material';
 import { converters } from './transformers';
 
 export interface ColorPickerProps {
 	onChange?: (value: string) => void;
-	convert: string;
+	convert: 'rgb' | 'rgba' | 'rgba_hex' | 'hex' | 'rgba_rgb';
 	internalValue?: string;
 	setValue?: (value: string) => void;
 	value?: string;
 }
 
-const ColorPicker = ({
-	convert,
-	...props
-}: ColorPickerProps) => {
+const ColorPicker = ({ convert, ...props }: ColorPickerProps) => {
 	const [showPicker, setShowPicker] = useState(false);
 	const [value, setValue] = useState(props?.value || '');
 
 	return (
 		<>
-			<Button variant="outlined" onClick={() => setShowPicker(true)}>
-				<div style={{ width: '20px', height: '20px', backgroundColor: value }} />
-			</Button>
+			<Grid container sx={{flexWrap:'nowrap'}}>
+				<Grid xs={3} item style={{ display: 'flex', alignItems:'stretch' }}>
+					<Button variant="outlined" onClick={() => setShowPicker(true)} sx={{borderColor:(theme) => 'rgb(17,17,17)'}}>
+						<div style={{ width: '26px', height: '26px', backgroundColor: value }} />
+					</Button>
+				</Grid>
+				<Grid xs={9} item>
+					<TextField value={value} variant="outlined" disabled={true} />
+				</Grid>
+			</Grid>
 			{showPicker && (
-				<div style={{ position: 'relative' }}>
+				<div style={{ position: 'relative', marginTop:0 }}>
 					<div style={{ position: 'absolute', zIndex: 1000 }}>
 						<div
 							style={{ position: 'fixed', top: '0px', right: '0px', bottom: '0px', left: '0px' }}
@@ -36,8 +40,6 @@ const ColorPicker = ({
 						<ChromePicker
 							color={value}
 							onChange={(c) => {
-								console.log('c', c);
-
 								const newValue = converters[convert](c);
 								setValue(newValue);
 								props?.onChange?.(newValue);
@@ -57,4 +59,3 @@ ColorPicker.defaultProps = {
 };
 
 export default ColorPicker;
-
