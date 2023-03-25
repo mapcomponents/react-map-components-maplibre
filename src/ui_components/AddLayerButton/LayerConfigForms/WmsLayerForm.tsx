e1cd9049eb959510878dd62e8cd9623b2957ccd7
Max Tobias Weber <maxtobiasweber@gmail.com>
2023-03-25 21:51:29 +0100

    add WmsLayerForm

---
+++ src/ui_components/AddLayerButton/LayerConfigForms/WmsLayerForm.tsx
@@ -0,0 +1,53 @@
+import {
+	Button,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	FormControl,
+	InputAdornment,
+	InputLabel,
+	OutlinedInput,
+} from '@mui/material';
+import React, { useMemo } from 'react';
+
+export interface WmsLayerConfig {
+	url: string;
+}
+
+export interface WmsLayerFormProps {
+	onSubmit: (config: WmsLayerConfig) => void;
+	onCancel: () => void;
+}
+
+export default function WmsLayerForm(props: WmsLayerFormProps) {
+	const [config, setConfig] = React.useState<{ url: string }>({ url: '' });
+
+	const configIsValid = useMemo(() => {
+		if (!config?.url) return false;
+
+		return true;
+	}, [config]);
+
+	return (
+		<>
+			<DialogTitle>WMS layer</DialogTitle>
+			<DialogContent>
+				<FormControl fullWidth sx={{ marginTop: '10px' }}>
+					<InputLabel htmlFor="wms-url-input">WMS URL</InputLabel>
+					<OutlinedInput
+						id="wms-url-input"
+						label="WMS URL"
+						value={config.url}
+						onChange={(ev) => setConfig({ ...config, url: ev.target.value })}
+					/>
+				</FormControl>
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={props.onCancel}>Cancel</Button>
+				<Button disabled={!configIsValid} onClick={() => props.onSubmit(config)}>
+					Add
+				</Button>
+			</DialogActions>
+		</>
+	);
+}
