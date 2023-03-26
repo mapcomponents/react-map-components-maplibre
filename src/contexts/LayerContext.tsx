f66a28a56f3356177f366ef6480882c5c0507376
Max Tobias Weber <maxtobiasweber@gmail.com>
2023-03-26 15:07:06 +0200

    add backgroundLayers and symbolLayer state variables to layerContext

--- src/contexts/LayerContext.tsx
+++ src/contexts/LayerContext.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MlVectorTileLayerProps } from 'src/components/MlVectorTileLayer/MlVectorTileLayer';
 
 export interface LayerContextProps {
 	children: React.ReactNode;
@@ -12,16 +13,34 @@ export interface LayerConfig {
 export interface LayerContextType {
 	layers: LayerConfig[];
 	setLayers: (layers: LayerConfig[] | ((layers: LayerConfig[]) => LayerConfig[])) => void;
+	backgroundLayers: MlVectorTileLayerProps['layers'];
+	setBackgroundLayers: (
+		layers:
+			| MlVectorTileLayerProps['layers']
+			| ((layers: MlVectorTileLayerProps['layers']) => MlVectorTileLayerProps['layers'])
+	) => void;
+	symbolLayers: MlVectorTileLayerProps['layers'];
+	setSymbolLayers: (
+		layers:
+			| MlVectorTileLayerProps['layers']
+			| ((layers: MlVectorTileLayerProps['layers']) => MlVectorTileLayerProps['layers'])
+	) => void;
 }
 
 const LayerContext = React.createContext({} as LayerContextType);
 
 function LayerContextProvider(props: LayerContextProps) {
 	const [layers, setLayers] = React.useState<any[]>([]);
+	const [backgroundLayers, setBackgroundLayers] = React.useState<any[]>([]);
+	const [symbolLayers, setSymbolLayers] = React.useState<any[]>([]);
 
 	const value = {
 		layers,
 		setLayers,
+		backgroundLayers,
+		setBackgroundLayers,
+		symbolLayers,
+		setSymbolLayers,
 	} as LayerContextType;
 
 	return <LayerContext.Provider value={value}>{props.children}</LayerContext.Provider>;
