f7e53598b4d3fcf6fb2c463e113d267b394ff350
mludwig <marwin.ludwig@wheregroup.com>
2023-05-10 12:17:25 +0200

    add Topbar, change test

--- src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
+++ src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
@@ -4,12 +4,16 @@ import { mount } from '@cypress/react';
 import * as stories from './MlTerrainLayer.stories';
 
 // compile the "Primary" story with the library
-const { ExampleConfig } = composeStories(stories);
+const { ExampleConfig }: any = composeStories(stories);
 
 describe('MlTerrainLayer Tests', () => {
 	it('Should ...', () => {
 		// and mount the story using @cypress/react library
 		mount(<ExampleConfig />);
+		cy.get('.terrainLayerButton').click();
 		cy.request('https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json');
+		cy.wait(5000);
+		cy.get('.terrainLayerButton').click();
+		cy.wait(5000);
 	});
 });
--- src/components/MlTerrainLayer/MlTerrainLayer.stories.tsx
+++ src/components/MlTerrainLayer/MlTerrainLayer.stories.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapLibreMap from '../MapLibreMap/MapLibreMap';
 import MlWmsLayer from '../MlWmsLayer/MlWmsLayer';
 import MlTerrainLayer from './MlTerrainLayer';
 import ThemeDecorator from '../../decorators/ThemeDecorator';
+import TopToolbar from '../../ui_components/TopToolbar';
+import { Button } from '@mui/material';
 
 const storyoptions = {
 	title: 'MapComponents/MlTerrainLayer',
@@ -13,6 +15,8 @@ const storyoptions = {
 export default storyoptions;
 
 const Template = () => {
+	const [start, setStart] = useState<boolean>(true);
+
 	const mapOptions: object = {
 		zoom: 4,
 		style: 'https://sgx.geodatenzentrum.de/gdz_basemapde_vektor/styles/bm_web_col.json',
@@ -20,18 +24,36 @@ const Template = () => {
 	};
 	return (
 		<>
+			<TopToolbar
+				unmovableButtons={
+					<>
+						<Button
+							variant={start ? 'contained' : 'outlined'}
+							className="terrainLayerButton"
+							onClick={() => setStart(!start)}
+						>
+							Terrain Layer
+						</Button>
+					</>
+				}
+			/>
 			<MapLibreMap options={mapOptions} mapId="map_1" />
-			<MlTerrainLayer
-				sourceOptions={{
-					tiles: ['https://vtc-cdn.maptoolkit.net/terrainrgb/{z}/{x}/{y}.webp'],
-				}}
-			/>
-			<MlWmsLayer
-				urlParameters={{
-					layers: 'de_basemapde_web_raster_hillshade',
-				}}
-				url="https://sgx.geodatenzentrum.de/wms_basemapde_schummerung"
-			/>
+
+			{start && (
+				<>
+					<MlTerrainLayer
+						sourceOptions={{
+							tiles: ['https://vtc-cdn.maptoolkit.net/terrainrgb/{z}/{x}/{y}.webp'],
+						}}
+					/>
+					<MlWmsLayer
+						urlParameters={{
+							layers: 'de_basemapde_web_raster_hillshade',
+						}}
+						url="https://sgx.geodatenzentrum.de/wms_basemapde_schummerung"
+					/>
+				</>
+			)}
 		</>
 	);
 };
