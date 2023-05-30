5b31f929ce6c3ec035275c12055ecf7ef7c95f97
mludwig <marwin.ludwig@wheregroup.com>
2023-05-30 11:32:16 +0200

    add thumbnail, add a working test

--- src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
+++ src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
@@ -7,12 +7,30 @@ import * as stories from './MlTerrainLayer.stories';
 const { ExampleConfig }: any = composeStories(stories);
 
 describe('MlTerrainLayer Tests', () => {
-	it('Should ...', () => {
-		// and mount the story using @cypress/react library
+	it('Should create terrain source, have hills layer, and set terrain property to null when Terrain Layer is not present', () => {
+		// Mount the story using @cypress/react library
 		mount(<ExampleConfig />);
+
+		cy.wait(3000);
+
+		// Access the map instance from the window object
+		cy.window().then((win) => {
+			const { _map } = win as { _map: any };
+			expect(_map?.style?.sourceCaches?.terrain).to.not.be.undefined;
+			expect(_map?.style?._layers?.hills).to.not.be.undefined;
+			expect(_map?.style?.terrain).to.not.be.null;
+		});
+
+		// Trigger the click event to toggle the Terrain Layer
 		cy.get('.terrainLayerButton').click();
-		cy.wait(5000);
-		cy.get('.terrainLayerButton').click();
-		cy.wait(5000);
+		cy.wait(2000);
+
+		// Access the map instance from the window object
+		cy.window().then((win) => {
+			const { _map } = win as { _map: any };
+			expect(_map?.style?.sourceCaches?.terrain).to.be.undefined;
+			expect(_map?.style?._layers?.hills).to.be.undefined;
+			expect(_map?.style?.terrain).to.be.null;
+		});
 	});
 });
