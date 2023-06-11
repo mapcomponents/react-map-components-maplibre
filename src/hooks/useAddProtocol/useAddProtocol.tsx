4790bbb67f03a3103d5074b54780557976de78a5
Max Tobias Weber <maxtobiasweber@gmail.com>
2023-06-11 12:35:54 +0200

    add useAddProtocol hook including mbtiles example

---
+++ src/hooks/useAddProtocol/useAddProtocol.tsx
@@ -0,0 +1,37 @@
+import maplibregl, { Cancelable, RequestParameters, ResponseCallback } from 'maplibre-gl';
+import { useEffect } from 'react';
+
+export interface useAddProtocolProps {
+	/**
+	 * the protocol string, for example `mbtiles`
+	 * In this example the handler function is called for all tile requests that start with the string `mbtiles://`
+	 */
+	protocol: string;
+	/**
+	 * Custom load tile function that will be called when using a source that starts with a custom url schema.
+	 */
+	handler: (requestParameters: RequestParameters, callback: ResponseCallback<any>) => Cancelable;
+}
+
+/**
+ * Enables the use of custom protocols (basically custom tile load functions) in the maplibre-gl-js library.
+ *
+ */
+const useAddProtocol = (props: useAddProtocolProps) => {
+	useEffect(() => {
+		if (!props.protocol || typeof props.handler !== 'function') return;
+
+		maplibregl.addProtocol(props.protocol, props.handler);
+
+		return () => {
+			maplibregl.removeProtocol(props.protocol);
+		};
+	}, []);
+
+	return {};
+};
+
+useAddProtocol.defaultProps = {
+	mapId: undefined,
+};
+export default useAddProtocol;
