50a3612fa151a578cafd506162a60c58a35d5db2
mludwig <marwin.ludwig@wheregroup.com>
2023-06-16 16:29:46 +0200

    change arrow Icon, add class

--- src/ui_components/LayerList/LayerListFolder.tsx
+++ src/ui_components/LayerList/LayerListFolder.tsx
@@ -1,4 +1,4 @@
-import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { KeyboardArrowRight as ExpandLess, ExpandMore } from '@mui/icons-material';
 import { ListItemIcon, ListItemText, List, Checkbox, ListItem, IconButton } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useMemo, useState } from 'react';
@@ -39,6 +39,7 @@ export default function LayerListFolder({ visible = true, name, children, setVis
 	return (
 		<>
 			<ListItem
+				className={'listItemFolder'}
 				sx={{
 					paddingRight: 0,
 					paddingLeft: 0,
@@ -48,11 +49,12 @@ export default function LayerListFolder({ visible = true, name, children, setVis
 			>
 				<ListItemIcon sx={{ minWidth: '30px' }}>
 					<IconButton edge="end" aria-label="open" onClick={() => setOpen(!open)}>
-						{open ? <ExpandLess /> : <ExpandMore />}
+						{open ? <ExpandMore /> : <ExpandLess />}
 					</IconButton>
 					<Checkbox
 						disabled={setVisible ? false : !visible}
 						checked={setVisible ? visible : localVisible}
+						sx={{ padding: 0 }}
 						onClick={() => {
 							if (setVisible) {
 								setVisible((val) => !val);
@@ -65,7 +67,7 @@ export default function LayerListFolder({ visible = true, name, children, setVis
 				<ListItemText primary={name} variant="layerlist" />
 			</ListItem>
 			<Box sx={{ display: open ? 'block' : 'none' }}>
-				<List component="div" disablePadding sx={{ paddingLeft: '70px' }}>
+				<List component="div" disablePadding sx={{ marginLeft: '25px' }}>
 					{_children}
 				</List>
 			</Box>
