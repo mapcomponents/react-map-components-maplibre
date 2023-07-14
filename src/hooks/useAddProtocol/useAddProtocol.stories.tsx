344ff4157ea4b5616f3e0b2dce3afc00b7a1ee30
Martin Alzueta <martin.alzueta@wheregroup.com>
2023-07-14 11:01:42 +0200

    csv handler

--- src/hooks/useAddProtocol/useAddProtocol.stories.tsx
+++ src/hooks/useAddProtocol/useAddProtocol.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useAddProtocol from './useAddProtocol';
 
@@ -6,6 +6,13 @@ import mapContextDecorator from '../../decorators/LowZoomDecorator';
 import MlVectorTileLayer from '../../components/MlVectorTileLayer/MlVectorTileLayer';
 import { LayerSpecification } from 'maplibre-gl';
 import { mbTilesProtocolHandler } from '../../protocol_handlers/mbtiles';
+//import MlGeoJsonLayer from '../../components/MlGeoJsonLayer/MlGeoJsonLayer';
+//import { FeatureCollection } from '@turf/turf';
+import { CSVProtocolHandler } from '../../protocol_handlers/csv';
+import useMap from '../useMap';
+import { Zoom } from '@mui/material';
+import MlLayer from 'src/components/MlLayer/MlLayer';
+import maplibregl from 'maplibre-gl';
 
 const storyoptions = {
 	title: 'hooks/useAddProtocol',
@@ -15,6 +22,54 @@ const storyoptions = {
 };
 export default storyoptions;
 
+const csvTemplate = () => {
+	const mapHook = useMap({ mapId: undefined });
+
+	useAddProtocol({
+		protocol: 'csv',
+		handler: CSVProtocolHandler,
+	});
+
+	useEffect(() => {
+		mapHook.map?.flyTo({ center: [-74.856923, 39.050793], zoom: 10, speed: 2 });
+		// mapHook.map?.addLayer({
+		// 	id: '000test',
+		// 	type: 'circle',
+		// 	source: 'csv://csv/restaurants.csv',
+		// 	//'source-layer': 'restaurants',
+		// 	minzoom: 0,
+		// 	maxzoom: 20,
+		// 	paint: {
+		// 		'circle-color': 'yellow',
+		// 		'circle-radius': 20,
+		// 	},
+		// });
+	}, [mapHook.map]);
+
+	return <>
+	<MlVectorTileLayer
+				mapId={'map_1'}
+				url={'csv://csv/restaurants.csv'}
+				layers={
+					[
+						{
+							id: 'restaurants',
+							type: 'circle',
+							'source-layer': 'restaurants',
+							layout: {},
+							paint: { 'circle-color': 'yellow','circle-radius': 20, },
+						},
+					] as unknown as LayerSpecification[]
+				}
+				
+				sourceOptions={{
+					type: 'vector',
+					minzoom: 4,
+					maxzoom: 18,
+				}}
+			/>
+	</>;
+};
 
 const Template = () => {
 	useAddProtocol({
@@ -49,6 +104,10 @@ const Template = () => {
 	);
 };
 
-export const ExampleConfig = Template.bind({});
-ExampleConfig.parameters = {};
-ExampleConfig.args = {};
+export const MbTiles = Template.bind({});
+MbTiles.parameters = {};
+MbTiles.args = {};
+
+export const CSV = csvTemplate.bind({});
+CSV.parameters = {};
+CSV.args = {};
--- src/protocol_handlers/mbtiles.ts
+++ src/protocol_handlers/mbtiles.ts
@@ -13,6 +13,7 @@ const parseParams = (url: string) => {
 	const x = mbtilesParts.splice(mbtilesPartsLength - 2, 1)[0];
 	const z = mbtilesParts.splice(mbtilesPartsLength - 3, 1)[0];
 	const filename = mbtilesParts.join('/');
+
 	return {
 		filename,
 		z,
@@ -59,11 +60,13 @@ async function getBufferFromMbtiles(params: { filename: string; z: string; x: st
 			// some of the logic here was heavily inspired by
 			// https://github.com/IsraelHikingMap/Site/blob/6aa2ec0cfb8891fa048b1d9e2a4fc7d4cbcc8c97/IsraelHiking.Web/src/application/services/database.service.ts
 			const result = db.exec(query);
+			console.log(result)
 			if (result.length !== 1) {
 				reject(new Error('Tile not found.'));
 				return;
 			}
 			const resultData = result[0].values[0][0] as Uint8Array;
+			
 			let binData: Uint8Array;
 			const isGzipped = resultData[0] === 0x1f && resultData[1] === 0x8b;
 			if (isGzipped) {
@@ -91,6 +94,7 @@ async function getBufferFromMbtiles(params: { filename: string; z: string; x: st
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const mbTilesProtocolHandler = (params: RequestParameters, callback: ResponseCallback<any>) => {
 	const parsedParams = parseParams(params.url);
+	
 	getBufferFromMbtiles(parsedParams).then((result) => {
 		if (result) {
 			callback(null, result, null, null);
@@ -98,6 +102,7 @@ const mbTilesProtocolHandler = (params: RequestParameters, callback: ResponseCal
 			callback(new Error('Tile not found'));
 		}
 	});
+
 	return { cancel: () => {} };
 };
 
