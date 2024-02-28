738929ba5609a0ff6b5c9b1f28fdb920d78e3661
Max Tobias Weber <tobias.weber@wheregroup.com>
2024-02-28 10:48:15 +0100

    ad makeMapContextDecorators function to enable changing MapLibreMap options inside the sb-story

--- src/components/MlFillExtrusionLayer/MlFillExtrusionLayer.stories.tsx
+++ src/components/MlFillExtrusionLayer/MlFillExtrusionLayer.stories.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 
 import MlFillExtrusionLayer, { MlFillExtrusionLayerProps } from './MlFillExtrusionLayer';
 
-import mapContextDecorator from '../../decorators/MapContextDecorator';
+import { makeMapContextDecorators } from '../../decorators/MapContextDecorator';
 
 const storyoptions = {
 	title: 'MapComponents/MlFillExtrusionLayer',
 	component: MlFillExtrusionLayer,
 	argTypes: {},
-	decorators: mapContextDecorator,
+	decorators: makeMapContextDecorators({
+		center: [13.738382110055795, 51.052846749921514],
+		bearing: 180,
+		zoom: 16.5,
+		pitch: 60
+	}),
 };
 export default storyoptions;
 
 const Template = (props: MlFillExtrusionLayerProps) => {
-
-		return <MlFillExtrusionLayer {...props} />;
+	return <MlFillExtrusionLayer {...props} />;
 };
 
 export const ExampleConfig = Template.bind({});
 ExampleConfig.parameters = {};
 ExampleConfig.args = {
 	paint: {
-		'fill-extrusion-color': "hsl(196, 61%, 83%)",
+		'fill-extrusion-color': 'hsl(196, 61%, 83%)',
 		'fill-extrusion-height': {
 			property: 'render_height',
 			type: 'identity',
@@ -33,14 +37,14 @@ ExampleConfig.args = {
 		'fill-extrusion-opacity': [
 			'interpolate',
 			// Set to interpoleta linearly between the pair of stops
-			["linear"],
+			['linear'],
 			['zoom'],
 			// When zoom is 13.5, buildings will be 100% transparent.
 			13.5,
 			0,
 			// When zoom is 15 or higher, buildings will be 100% opaque.
 			14.5,
-			1
-			],
+			1,
+		],
 	},
 };
