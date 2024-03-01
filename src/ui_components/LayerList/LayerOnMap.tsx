967fdbdc1b638a9b3187b19b256273dfb64b6f8d
Fabian Polakowski <fabian.polakowski@wheregroup.com>
2024-03-01 15:52:01 +0100

    rearrange layers in map if order changes

--- src/ui_components/LayerList/LayerOnMap.tsx
+++ src/ui_components/LayerList/LayerOnMap.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../stores/map.store';
+import { LayerConfig, RootState } from '../../stores/map.store';
 import MlGeoJsonLayer from '../../components/MlGeoJsonLayer/MlGeoJsonLayer';
 import { Feature } from '@turf/turf';
+import MlOrderLayers from '../../components/MlOrderLayers/MlOrderLayers';
 
 interface LayerOnMapProps {
 	mapConfigUuid: string;
@@ -12,12 +13,44 @@ function LayerOnMap(props: LayerOnMapProps) {
 	const layers = useSelector(
 		(state: RootState) => state.mapConfig.mapConfigs[props.mapConfigUuid].layers
 	);
+	const layerStoreOrder = useSelector(
+		(state: RootState) => state.mapConfig.mapConfigs[props.mapConfigUuid].layerOrder
+	);
+	const layerStoreIds = Object.values(layers).map((layer) => layer.uuid);
+	const layerStoreLength = Object.keys(layers).length;
+
+	const orderLayers = useMemo(() => {
+		const layerIds = [
+			'order-background',
+			...[...layerStoreIds].map((_el, idx) => 'layer_number_' + idx),
+			'order-labels',
+		];
+		return layerIds.reverse();
+	}, [layerStoreOrder]);
+
+	const sortedLayers = useMemo(() => {
+		const sorted: { [key: string]: LayerConfig } = {};
+		layerStoreOrder.forEach((orderItem) => {
+			Object.entries(layers).forEach(([key, layer]) => {
+				if (layer.uuid === orderItem.uuid) {
+					sorted[key] = layer;
+				}
+			});
+		});
+		return sorted;
+	}, [layers, layerStoreOrder]);
+
 	return (
 		<>
-			{Object.entries(layers).map(([key, layer]) => {
+			<MlOrderLayers layerIds={orderLayers}></MlOrderLayers>
+			{Object.entries(sortedLayers).map(([key, layer], index) => {
 				if (layer.type === 'geojson') {
 					return (
-						<MlGeoJsonLayer key={key} geojson={layer.config.geojson as Feature}></MlGeoJsonLayer>
+						<MlGeoJsonLayer
+							key={key}
+							geojson={layer.config.geojson as Feature}
+							insertBeforeLayer={'layer_number_' + (layerStoreLength - 1 - index)}
+						></MlGeoJsonLayer>
 					);
 				} else {
 					//TODO: handle all other layer types
