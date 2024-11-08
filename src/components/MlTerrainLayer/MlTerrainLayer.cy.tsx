2b8f83337aa444dfcebd6565b8e9e4fe151be4b0
Fabian Polakowski <fabian.polakowski@wheregroup.com>
2024-11-08 15:11:44 +0100

    get rid of cy.wait; wait for _map object instead

--- src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
+++ src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
@@ -11,26 +11,27 @@ describe('MlTerrainLayer Tests', () => {
 		// Mount the story using @cypress/react library
 		mount(<ExampleConfig />);
 
-		cy.wait(3000);
-
-		// Access the map instance from the window object
-		cy.window().then((win) => {
-			const { _map }: any = win;
-			expect(_map?.style?.sourceCaches?.terrain).to.not.be.undefined;
-			expect(_map?.style?._layers?.hills).to.not.be.undefined;
-			expect(_map?.style?.terrain).to.not.be.null;
-		});
+		// Wait for the map to be initialized and verify the terrain properties
+		cy.window()
+			.should('have.property', '_map')
+			.then((win) => {
+				const { _map }: any = win;
+				expect(_map?.style?.sourceCaches?.terrain).to.not.be.undefined;
+				expect(_map?.style?._layers?.hills).to.not.be.undefined;
+				expect(_map?.style?.terrain).to.not.be.null;
+			});
 
 		// Trigger the click event to toggle the Terrain Layer
 		cy.get('.terrainLayerButton').click();
-		cy.wait(2000);
 
-		// Access the map instance from the window object
-		cy.window().then((win) => {
-			const { _map }: any = win;
-			expect(_map?.style?.sourceCaches?.terrain).to.be.undefined;
-			expect(_map?.style?._layers?.hills).to.be.undefined;
-			expect(_map?.style?.terrain).to.be.null;
-		});
+		// Wait for the map to update and verify the terrain properties again
+		cy.window()
+			.should('have.property', '_map')
+			.then((win) => {
+				const { _map }: any = win;
+				expect(_map?.style?.sourceCaches?.terrain).to.be.undefined;
+				expect(_map?.style?._layers?.hills).to.be.undefined;
+				expect(_map?.style?.terrain).to.be.null;
+			});
 	});
 });
