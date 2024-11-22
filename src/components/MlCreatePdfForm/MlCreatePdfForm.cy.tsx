bc6d04012aaef3df5a4daece558f4628fdce4220
Fabian Polakowski <f.polakowski@gmx.de>
2024-11-22 16:37:36 +0100

    update to cypress/react18

--- src/components/MlCreatePdfForm/MlCreatePdfForm.cy.tsx
+++ src/components/MlCreatePdfForm/MlCreatePdfForm.cy.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
-import { mount } from '@cypress/react';
-import * as stories from "./MlCreatePdfForm.stories";
+import { mount } from '@cypress/react18';
+import * as stories from './MlCreatePdfForm.stories';
 
 // compile the "Primary" story with the library
-const {ExampleConfig } = composeStories(stories);
+const { ExampleConfig }: any = composeStories(stories);
 
 describe('MlCreatePdfForm Tests', () => {
 	it('Should generate and download a PDF export of the current map preview', () => {
 		// and mount the story using @cypress/react library
 		mount(<ExampleConfig />);
 
-    cy.get(".pdfFormButton").click();
- 
-		cy.wait(1000)
-    cy.get(".createPdfButton").click();
-		cy.wait(5000)
-    cy.readFile("./cypress/downloads/Map.pdf").should('contain', 'WhereGroup')
+		cy.get('.pdfFormButton').click();
+
+		cy.wait(1000);
+		cy.get('.createPdfButton').click();
+		cy.wait(5000);
+		cy.readFile('./cypress/downloads/Map.pdf').should('contain', 'WhereGroup');
 	});
 });
