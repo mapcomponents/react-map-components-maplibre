135a24d318e245c6ebda6dae0e1557e2fe9b1091
Fabian Polakowski <f.polakowski@gmx.de>
2024-11-22 16:37:17 +0100

    fix terrainlayer test

--- src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
+++ src/components/MlTerrainLayer/MlTerrainLayer.cy.tsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
-import { mount } from '@cypress/react';
+import { mount } from '@cypress/react18';
 import * as stories from './MlTerrainLayer.stories';
 
-// compile the "Primary" story with the library
 const { ExampleConfig }: any = composeStories(stories);
 
 describe('MlTerrainLayer Tests', () => {
 	it('Should create terrain source, have hills layer, and set terrain property to null when Terrain Layer is not present', () => {
-		// Mount the story using @cypress/react library
 		mount(<ExampleConfig />);
 
-		// Wait for the map to be initialized and verify the terrain properties
+		// Wait for the map to be initialized and verify, that the terrain layer is added to the map
 		cy.window()
-			.should('have.property', '_map')
+			.should((win) => {
+				expect((win as any)._map).to.exist;
+			})
 			.then((win) => {
 				const { _map }: any = win;
 				expect(_map?.style?.sourceCaches?.terrain).to.not.be.undefined;
 				expect(_map?.style?._layers?.hills).to.not.be.undefined;
-				expect(_map?.style?.terrain).to.not.be.null;
 			});
 
-		// Trigger the click event to toggle the Terrain Layer
+		// Trigger the click event to turn the terrain layer off
 		cy.get('.terrainLayerButton').click();
 
-		// Wait for the map to update and verify the terrain properties again
+		// Dynamically wait for the map to update and verify terrain removal
 		cy.window()
-			.should('have.property', '_map')
+			.should((win) => {
+				expect((win as any)._map).to.exist;
+			})
 			.then((win) => {
-				const { _map }: any = win;
-				expect(_map?.style?.sourceCaches?.terrain).to.be.undefined;
-				expect(_map?.style?._layers?.hills).to.be.undefined;
-				expect(_map?.style?.terrain).to.be.null;
+				cy.wrap(win).should((win) => {
+					const { _map }: any = win;
+					expect(_map?.style?.sourceCaches?.terrain).to.be.undefined;
+					expect(_map?.style?._layers?.hills).to.be.undefined;
+				});
 			});
 	});
 });
