c1151f67c10f818516dfd9d47c769f97cc51add2
jannik brack <jannik.brack@wheregroup.com>
2025-03-11 11:33:23 +0100

    ignor import error from module osm2geojson-lite

--- src/protocol_handlers/osm.ts
+++ src/protocol_handlers/osm.ts
@@ -1,5 +1,7 @@
 import { RequestParameters} from 'maplibre-gl';
 import { FeatureCollection } from 'geojson';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import osm2geojson from 'osm2geojson-lite';
 import protocolPathParser from './utils/protocolPathParser';
 import getProtocolData from './utils/getProtocolData';
