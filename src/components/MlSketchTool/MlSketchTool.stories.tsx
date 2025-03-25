9d3f5edd66a3fc1bb22de7a038e0e8dda45c1a6b
lhackenjos <lena.hackenjos@wheregroup.com>
2025-03-25 15:59:49 +0100

    remove old instruction Box

--- src/components/MlSketchTool/MlSketchTool.stories.tsx
+++ src/components/MlSketchTool/MlSketchTool.stories.tsx
@@ -60,28 +60,6 @@ const catalgoueTemplate = () => {
 
 	return (
 		<>
-			{/* <Box
-				sx={{
-					position: 'fixed',
-					width: { xs: '100%', sm: 'auto' },
-					top: { xs: '62px', sm: '22px' },
-					right: { xs: '0px', sm: '180px' },
-					paddingRight: { xs: '20px', sm: '0px' },
-					color: '#009ee0',
-					backgroundColor: '#fff',
-					textAlign: 'right',
-					fontSize: '16px',
-					fontFamily: 'sans-serif',
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '5px',
-					zIndex: 5000,
-				}}
-			>
-				{mediaIsMobile
-					? 'Zum Beenden erneut auf denselben Punkt klicken.'
-					: 'Die Zeichnung kann beendet werden, indem erneut auf den zuletzt gezeichneten Punkt geklickt wird.'}
-			</Box> */}
 			<TopToolbar
 				buttons={
 					<>
