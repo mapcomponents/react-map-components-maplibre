3ea65bec1e757723111bc94abf84f53a7a3a7e3c
jannik brack <jannik.brack@wheregroup.com>
2025-03-27 15:58:09 +0100

    Fix MlLayerSwipe.test.js change svg to react component in MlLayerSwipe.tsx

--- src/components/MlLayerSwipe/MlLayerSwipe.test.js
+++ src/components/MlLayerSwipe/MlLayerSwipe.test.js
@@ -1,17 +1,18 @@
-import MlLayerSwipe from "./MlLayerSwipe";
+import MlLayerSwipe from './MlLayerSwipe';
+import React, { useContext, useState } from 'react';
+import MapContext, { MapComponentsProvider } from '../../contexts/MapContext';
+import MapLibreMap from './../MapLibreMap/MapLibreMap';
+import syncMove from '@mapbox/mapbox-gl-sync-move';
+import userEvent from '@testing-library/user-event';
+import {render, screen} from '@testing-library/react';
 
-import React, { useContext, useState } from "react";
-import { mount } from "enzyme";
-import MapContext, { MapComponentsProvider } from "../../contexts/MapContext";
-import MapLibreMap from "./../MapLibreMap/MapLibreMap";
-import syncMove from "@mapbox/mapbox-gl-sync-move";
 // Mapbox sync-move mockup
 var mockSyncMoveMethods = {
 	cleanup: jest.fn(),
 };
 
-jest.mock("@mapbox/mapbox-gl-sync-move", () => {
-	const originalModule = jest.requireActual("@mapbox/mapbox-gl-sync-move");
+jest.mock('@mapbox/mapbox-gl-sync-move', () => {
+	const originalModule = jest.requireActual('@mapbox/mapbox-gl-sync-move');
 	return {
 		__esModule: true,
 		...originalModule,
@@ -23,20 +24,21 @@ const syncMoveObj = {
 	syncMove: syncMove,
 };
 
-const TestComponent = (props) => {
+const TestComponent = () => {
 	const [layerVisible, setLayerVisible] = useState(true);
 	const [refreshTrigger, setRefreshTrigger] = useState(0);
 	const mapContext = useContext(MapContext);
 
 	return (
 		<>
-			<MapLibreMap mapId="map_1" />
-			<MapLibreMap mapId="map_2" />
+			<MapLibreMap mapId="map_1" data-testid="map_1" />
+			<MapLibreMap mapId="map_2" data-testid="map_2" />
 
 			{layerVisible && <MlLayerSwipe map1Id="map_1" map2Id="map_2" />}
 
 			<button
 				className="toggle_layer_visible"
+				data-testid="toggle_layer_visible"
 				onClick={() => {
 					setLayerVisible(!layerVisible);
 				}}
@@ -45,27 +47,22 @@ const TestComponent = (props) => {
 			</button>
 			<button
 				className="trigger_refresh"
+				data-testid="trigger_refresh"
 				onClick={() => {
 					setRefreshTrigger(refreshTrigger + 1);
 				}}
 			>
 				refresh
 			</button>
-			<div className="layers_json">
+			<div className="layers_json" data-testid="layers_json">
 				{mapContext.map && refreshTrigger && JSON.stringify(mapContext.map.layers)}
 			</div>
 		</>
 	);
 };
 
-const createWrapper = () =>
-	mount(
-		<MapComponentsProvider>
-			<TestComponent />
-		</MapComponentsProvider>
-	);
 
-describe("<MlLayerSwipe/>", () => {
+describe('<MlLayerSwipe/>', () => {
 	//it("should add a MlLayerSwipe Component to that calls syncMaps with both available MapLibre instances once", async () => {
 	//	const spy = jest.spyOn(syncMoveObj, "syncMove");
 	//	const wrapper = createWrapper(TestComponent);
@@ -73,12 +70,15 @@ describe("<MlLayerSwipe/>", () => {
 	//	expect(spy).toHaveBeenCalledTimes(1);
 	//});
 
-	it("should call the syncMaps cleanup function once when it is removed from reactDOM", async () => {
-		const wrapper = createWrapper(TestComponent);
-
+	it('should call the syncMaps cleanup function once when it is removed from reactDOM', async () => {
+		render(
+			<MapComponentsProvider>
+				<TestComponent />
+			</MapComponentsProvider>
+		);
 		//expect(syncMove).toHaveBeenCalledTimes(1);
 
-		wrapper.find(".toggle_layer_visible").simulate("click");
+		await userEvent.click(screen.getByTestId('toggle_layer_visible'))
 
 		//TODO: Fix cleanup test
 		expect(mockSyncMoveMethods.cleanup).toHaveBeenCalledTimes(1);
