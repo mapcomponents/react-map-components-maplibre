eec42f2027c2cd71c656fe9ee6e3f3df6ac986fc
Martin Alzueta <martin.alzueta@wheregroup.com>
2025-04-29 09:45:40 +0200

    fixed merge errors

--- src/components/MlGeoJsonLayer/util/getDefaultPaintPropsByType.ts
+++ src/components/MlGeoJsonLayer/util/getDefaultPaintPropsByType.ts
@@ -13,15 +13,10 @@ const getDefaultPaintPropsByType = (
 				// 'fill-color': 'rgba(10,240,256,0.6)',
 				// 'fill-outline-color': 'rgba(20,230,256,0.8)',
 				'fill-color': '#009EE0',
-				'fill-outline-color': '#009EE0',
-				// 'fill-color': 'rgba(10,240,256,0.6)',
-				// 'fill-outline-color': 'rgba(20,230,256,0.8)',
-				'fill-color': '#009EE0',
-				'fill-outline-color': '#009EE0',
+				'fill-outline-color': '#009EE0'
 			};
 		case 'line':
 			if (defaultPaintOverrides?.line) {
-				console.log(defaultPaintOverrides.line);
 				console.log(defaultPaintOverrides.line);
 				return defaultPaintOverrides.line;
 			}
@@ -43,15 +38,10 @@ const getDefaultPaintPropsByType = (
 				// 'circle-stroke-width': 2,
 				// 'circle-radius': 4,
 				'circle-color': '#009EE0',
-				// 'circle-color': 'rgba(10,240,256,0.8)',
-				// 'circle-stroke-color': '#fff',
-				// 'circle-stroke-width': 2,
-				// 'circle-radius': 4,
-				'circle-color': '#009EE0',
 				'circle-stroke-color': '#fff',
 				'circle-stroke-width': 2,
 				'circle-radius': 5,
-				'circle-radius': 5,
+		
 			};
 		default:
 			return {};
