655b556339a521afe8bc089719d4d2e8792ca2db
lhackenjos <lena.hackenjos@wheregroup.com>
2025-06-23 09:10:02 +0200

    Add _map.to.exist line to every it-test in .cy file

--- src/components/MlGlobeButton/MlGlobeButton.cy.tsx
+++ src/components/MlGlobeButton/MlGlobeButton.cy.tsx
@@ -8,38 +8,41 @@ const { CatalogueDemo }: any = composeStories(stories);
 
 describe('MlGlobeButton', () => {
 	
-		it('shows MapIcon as start state and changes to PublicIcon after click', () => {
-		mount(<CatalogueDemo/>)
-		cy.window()
-			.should((win) => expect((win as any)._map).to.exist)
-			.then(() => {
-				cy.get('[data-testid="MapIcon"]').should('exist');
-				cy.get('[data-testid="PublicIcon"]').should('not.exist');
-				cy.get('button').click();
-				cy.get('[data-testid="PublicIcon"]').should('exist');
-				cy.get('[data-testid="MapIcon"]').should('not.exist');
-			})
+	it('shows MapIcon as start state and changes to PublicIcon after click', () => {
+		mount(<CatalogueDemo />);
+		cy.window().should((win) => expect((win as any)._map).to.exist);
+
+		cy.get('[data-testid="MapIcon"]').should('exist');
+		cy.get('[data-testid="PublicIcon"]').should('not.exist');
+		cy.get('button').click();
+		cy.get('[data-testid="PublicIcon"]').should('exist');
+		cy.get('[data-testid="MapIcon"]').should('not.exist');
 	});
 
-	// it('toggles between Globe and Mercator when clicked again', () => {
-	// 	mountWithTheme(<MlGlobeButton />);
-	// 	cy.get('button').click(); // Switches to Globe (PublicIcon)
-	// 	cy.get('[data-testid="PublicIcon"]').should('exist');
-	// 	cy.get('button').click(); // Switches back to Mercator (MapIcon)
-	// 	cy.get('[data-testid="MapIcon"]').should('exist');
-	// });
+	it('toggles between Globe and Mercator when clicked again', () => {
+		mount(<CatalogueDemo />);
+		cy.window().should((win) => expect((win as any)._map).to.exist);
 
-	// it('changes the projection on the map instance attached to window', () => {
-	// 	mountWithTheme(<MlGlobeButton />);
-	// 	cy.get('button').click();
-	// 	cy.window().then((win) => {
-	// 		const map = (win as any)._map;
-	// 		expect(map.getProjection().type).to.eq('globe');
-	// 	});
-	// 	cy.get('button').click();
-	// 	cy.window().then((win) => {
-	// 		const map = (win as any)._map;
-	// 		expect(map.getProjection().type).to.eq('mercator');
-	// 	});
-	// });
+		cy.get('button').click(); // Switches to Globe (PublicIcon)
+		cy.get('[data-testid="PublicIcon"]').should('exist');
+		cy.get('button').click(); // Switches back to Mercator (MapIcon)
+		cy.get('[data-testid="MapIcon"]').should('exist');
+	});
+
+	it('changes the projection on the map instance attached to window', () => {
+		mount(<CatalogueDemo />);
+		cy.window().should((win) => expect((win as any)._map).to.exist);
+
+		cy.get('button').click();
+		cy.window().should((win) => {
+			const map = (win as any)._map;
+			expect(map.getProjection().type).to.eq('globe');
+		});
+
+		cy.get('button').click();
+		cy.window().should((win) => {
+			const map = (win as any)._map;
+			expect(map.getProjection().type).to.eq('mercator');
+		});
+	});
 });
