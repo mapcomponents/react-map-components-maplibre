2cc2c533c1655533355a624583a7d25091f02f80
cioddi <maxtobiasweber@gmail.com>
2025-10-16 14:09:39 +0000

    Deploying to docs from  @ c5faf0a76b234509aab1dd1c986c940d5a9c4259 🚀

---
+++ docs/react-maplibre/assets/MlMultiMeasureTool.stories-Cw-fVPbl.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-DPYK5RoS.js";import{am as o}from"./style-CfZ1jwv6.js";import{m as t}from"./MapContextDecorator-CyXBN-uO.js";import"./iframe-DdvSZNba.js";import"./preload-helper-D9Z9MdNV.js";import"./index-BJOkD4WZ.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <MlMultiMeasureTool multiType={true} />
+        </>;
+}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
