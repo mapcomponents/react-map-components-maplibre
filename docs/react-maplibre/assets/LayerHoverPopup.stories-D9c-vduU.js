5f5a3ade901d4b30fad27bf242f2794767892459
cioddi <maxtobiasweber@gmail.com>
2025-11-01 12:16:30 +0000

    Deploying to docs from  @ f102af52cfa0494840659149de9bf4e647d7024c 🚀

---
+++ docs/react-maplibre/assets/LayerHoverPopup.stories-D9c-vduU.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-BOLpfN_U.js";import{b8 as e}from"./style-BZ-McjXS.js";import{m as t}from"./MapContextDecorator-DB23OuZF.js";import"./iframe-Nh0wwQVS.js";import"./preload-helper-D9Z9MdNV.js";import"./index-B10VXxfN.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
+        </>;
+}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
---
+++ docs/react-maplibre/assets/MlImageMarkerLayer.stories-gh1rUw5e.js
@@ -0,0 +1,20 @@
+import{j as r}from"./index-BOLpfN_U.js";import{a2 as o}from"./style-BZ-McjXS.js";import{m as t}from"./MapContextDecorator-DB23OuZF.js";import"./iframe-Nh0wwQVS.js";import"./preload-helper-D9Z9MdNV.js";import"./index-B10VXxfN.js";const l={title:"MapComponents/MlImageMarkerLayer",component:o,argTypes:{},decorators:t},a=()=>r.jsx(o,{options:{source:{type:"geojson",data:{type:"Feature",geometry:{type:"Point",coordinates:[7.0847929969609424,50.73855193187643]},properties:{id:"test"}}},layout:{"icon-allow-overlap":!0,"icon-size":.14,"icon-offset":[0,-180]}},imgSrc:"assets/marker.png"}),e=a.bind({});e.parameters={};e.args={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`() => <MlImageMarkerLayer options={{
+  source: {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [7.0847929969609424, 50.73855193187643]
+      },
+      properties: {
+        id: 'test'
+      }
+    }
+  },
+  layout: {
+    'icon-allow-overlap': true,
+    'icon-size': 0.14,
+    'icon-offset': [0, -180]
+  }
+}} imgSrc={'assets/marker.png'} />`,...e.parameters?.docs?.source}}};const g=["ExampleConfig"];export{e as ExampleConfig,g as __namedExportsOrder,l as default};
