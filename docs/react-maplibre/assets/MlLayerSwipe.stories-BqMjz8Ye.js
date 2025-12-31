f26fb51856a356097e555dd7ed575488f8e5a9de
cioddi <maxtobiasweber@gmail.com>
2025-12-31 09:18:53 +0000

    Deploying to docs from  @ 26ea174ce63caf0b5c34c05f0f49fa4e24569dbf 🚀

--- docs/react-maplibre/assets/MlLayerSwipe.stories-BqMjz8Ye.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./index-BGUai4Zw.js";import{a5 as e,a4 as s}from"./style-xrvswyc8.js";import{d as m}from"./MultiMapContextDecorator-pASoLHnU.js";import"./iframe-DmqzQxjR.js";import"./preload-helper-D9Z9MdNV.js";import"./index-ZOTdVa-Q.js";const u={title:"MapComponents/MlLayerSwipe",component:e,argTypes:{},decorators:m},t=()=>r.jsxs(r.Fragment,{children:[r.jsx(s,{visible:!0,url:"https://www.wms.nrw.de/geobasis/wms_nw_uraufnahme",urlParameters:{layers:"nw_uraufnahme_rw"},mapId:"map_2"}),r.jsx(e,{map1Id:"map_1",map2Id:"map_2",buttonStyle:void 0})]}),a=t.bind({});a.parameters={};a.args={};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`() => <>
-        <MlWmsLayer visible url="https://www.wms.nrw.de/geobasis/wms_nw_uraufnahme" urlParameters={{
-    layers: 'nw_uraufnahme_rw'
-  }} mapId="map_2" />
-        <MlLayerSwipe map1Id="map_1" map2Id="map_2" buttonStyle={undefined} />
-    </>`,...a.parameters?.docs?.source}}};const w=["ExampleConfig"];export{a as ExampleConfig,w as __namedExportsOrder,u as default};
