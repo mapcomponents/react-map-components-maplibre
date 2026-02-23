c713dc280572f7a87eecf24fbe9b27d6abf2af53
JannikBrack <88673060+JannikBrack@users.noreply.github.com>
2026-02-23 11:21:44 +0000

    Deploying to docs from  @ 0eec3ec9b1dff556c2af5e390ce683db78038d5f 🚀

---
+++ docs/react-maplibre/assets/MlImageMarkerLayer.stories-BY0fV6O7.js
@@ -0,0 +1,20 @@
+import{j as r}from"./index-DjFac3ux.js";import{a2 as o}from"./style-B-c0kfUt.js";import{m as t}from"./MapContextDecorator-B0jn_jf8.js";import"./iframe-BKTRN4sA.js";import"./preload-helper-D9Z9MdNV.js";import"./index-CDy4GDEb.js";const l={title:"MapComponents/MlImageMarkerLayer",component:o,argTypes:{},decorators:t},a=()=>r.jsx(o,{options:{source:{type:"geojson",data:{type:"Feature",geometry:{type:"Point",coordinates:[7.0847929969609424,50.73855193187643]},properties:{id:"test"}}},layout:{"icon-allow-overlap":!0,"icon-size":.14,"icon-offset":[0,-180]}},imgSrc:"assets/marker.png"}),e=a.bind({});e.parameters={};e.args={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`() => <MlImageMarkerLayer options={{
+  source: {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [7.0847929969609424, 50.73855193187643]
+      },
+      properties: {
+        id: 'test'
+      }
+    }
+  },
+  layout: {
+    'icon-allow-overlap': true,
+    'icon-size': 0.14,
+    'icon-offset': [0, -180]
+  }
+}} imgSrc={'assets/marker.png'} />`,...e.parameters?.docs?.source}}};const g=["ExampleConfig"];export{e as ExampleConfig,g as __namedExportsOrder,l as default};
