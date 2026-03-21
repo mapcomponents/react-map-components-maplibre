51546a1f09306383479375eecbe229ef868dda99
cioddi <maxtobiasweber@gmail.com>
2026-03-21 18:59:32 +0000

    Deploying to docs from  @ dcf92c502459ac8cb71c68619fd678fa9ec68e28 🚀

---
+++ docs/react-maplibre/assets/MlMultiMeasureTool.stories-BiQqqbl-.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-nTF3L2P0.js";import{am as o}from"./style-BD_ZMq66.js";import{m as t}from"./MapContextDecorator-BpDKTTc4.js";import"./iframe-B2nQNTi0.js";import"./preload-helper-D9Z9MdNV.js";import"./index-DbztUEaH.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <MlMultiMeasureTool multiType={true} />
+        </>;
+}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
--- docs/react-maplibre/assets/MlMultiMeasureTool.stories-C8lgg0vH.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./index-D4H7B5kW.js";import{am as o}from"./style-jSEnKgMX.js";import{m as t}from"./MapContextDecorator-CempxDSD.js";import"./iframe-Nes0rx_D.js";import"./preload-helper-D9Z9MdNV.js";import"./index-C5bZ_phi.js";const u={title:"MapComponents/MlMultiMeasureTool",component:o,argTypes:{},decorators:t},s=()=>e.jsx(e.Fragment,{children:e.jsx(o,{multiType:!0})}),r=s.bind({});r.parameters={};r.args={};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <MlMultiMeasureTool multiType={true} />
-        </>;
-}`,...r.parameters?.docs?.source}}};const c=["ExampleConfig"];export{r as ExampleConfig,c as __namedExportsOrder,u as default};
