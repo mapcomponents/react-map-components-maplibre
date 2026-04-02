3c3658b31cc2eac4a698127230d55795abd89af9
cioddi <maxtobiasweber@gmail.com>
2026-04-02 12:20:10 +0000

    Deploying to docs from  @ d7e76dc789063f6024899af82c12b1cb04454420 🚀

---
+++ docs/react-maplibre/assets/LayerHoverPopup.stories-B4wCCMkO.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-DSp5Wjp3.js";import{b9 as e}from"./style-d9wbuOGE.js";import{m as t}from"./MapContextDecorator-CAZvmHeV.js";import"./iframe-DujiTvTV.js";import"./preload-helper-D9Z9MdNV.js";import"./index-Dn6GSo6H.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
+        </>;
+}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
--- docs/react-maplibre/assets/LayerHoverPopup.stories-DeOUBYF3.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./index-B7aaknr0.js";import{b9 as e}from"./style-D5yzXf6L.js";import{m as t}from"./MapContextDecorator-tSmhV23B.js";import"./iframe-BeZyBdkx.js";import"./preload-helper-D9Z9MdNV.js";import"./index-Bb9Y8FxW.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
-        </>;
-}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
