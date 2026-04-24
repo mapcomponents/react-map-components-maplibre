89cd02d70ce2f8f766fe28214cfe19751ce231ef
jannik brack <jannik.brack@wheregroup.com>
2026-04-24 14:45:37 +0200

    fix formating

--- apps/template/src/main.tsx
+++ apps/template/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MapComponentsProvider } from "@mapcomponents/react-maplibre";
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MapComponentsProvider } from '@mapcomponents/react-maplibre';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-   <MapComponentsProvider>
-      <App />
-    </MapComponentsProvider>
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<MapComponentsProvider>
+			<App />
+		</MapComponentsProvider>
+	</React.StrictMode>
+);
