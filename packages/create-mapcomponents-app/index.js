ce4080316a1660a5ce009f00fbce943a342395ef
jannik brack <jannik.brack@wheregroup.com>
2026-04-25 07:19:26 +0200

    update link to clone template

--- packages/create-mapcomponents-app/index.js
+++ packages/create-mapcomponents-app/index.js
@@ -3,7 +3,9 @@
 import degit from 'degit';
 const path = process.argv[2] || 'my-mapcomponents-app';
 
-const emitter = degit('mapcomponents/template');
+const emitter = degit(
+	'mapcomponents/react-map-components-maplibre/tree/main/apps/template'
+);
 emitter.clone(path).then(() => {
 	console.log(`Project cloned to ${path}`);
 });
