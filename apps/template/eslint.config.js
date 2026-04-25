21628cdc747ee7c40bc9c0c4f700dfe36bc6d702
jannik brack <jannik.brack@wheregroup.com>
2026-04-25 07:40:03 +0200

    fix: formation

--- apps/template/eslint.config.js
+++ apps/template/eslint.config.js
@@ -24,10 +24,7 @@ export default tseslint.config(
 		},
 		rules: {
 			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': [
-				'warn',
-				{ allowConstantExport: true },
-			],
+			'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		},
 	}
 );
