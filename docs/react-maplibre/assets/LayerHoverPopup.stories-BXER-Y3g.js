ea98c60beed82e23ad600d6bfcbfd93f5c2e5b38
JannikBrack <88673060+JannikBrack@users.noreply.github.com>
2026-05-04 09:36:42 +0000

    Deploying to docs from  @ 37162d905ba920a5105df300c53e0087032a30ea 🚀

---
+++ docs/react-maplibre/assets/LayerHoverPopup.stories-BXER-Y3g.js
@@ -0,0 +1,5 @@
+import{j as r}from"./index-B0N87h5Q.js";import{b9 as e}from"./style-B3HIvMgn.js";import{m as t}from"./MapContextDecorator-CO5D-j6K.js";import"./iframe-UJ4usrXp.js";import"./preload-helper-D9Z9MdNV.js";import"./index-Bf9Jd50O.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
+  return <>
+            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
+        </>;
+}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
--- docs/react-maplibre/assets/LayerHoverPopup.stories-C0wiPHQU.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./index-CSPMo6W0.js";import{b9 as e}from"./style-CT2DeGn4.js";import{m as t}from"./MapContextDecorator-LkjE0Guu.js";import"./iframe-DY9T1kbv.js";import"./preload-helper-D9Z9MdNV.js";import"./index-BtgaYbTv.js";const u={title:"Hooks/LayerHoverPopup",component:e,argTypes:{},decorators:t},p=()=>r.jsx(r.Fragment,{children:r.jsx(e,{getPopupContent:()=>"Popup content"})}),o=p.bind({});o.parameters={};o.args={};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => {
-  return <>
-            <LayerHoverPopup getPopupContent={() => 'Popup content'} />
-        </>;
-}`,...o.parameters?.docs?.source}}};const d=["ExampleConfig"];export{o as ExampleConfig,d as __namedExportsOrder,u as default};
