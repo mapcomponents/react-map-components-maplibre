422d3b928da503f84aa1493b52ccc8a9eb826444
jannik brack <jannik.brack@wheregroup.com>
2026-05-07 15:26:26 +0200

    Update Storybook refs for development and production environments

--- apps/storybook-composition/.storybook/main.ts
+++ apps/storybook-composition/.storybook/main.ts
@@ -12,7 +12,46 @@ const config: StorybookConfig = {
 		},
 	},
 	staticDirs: ['../public'],
-
+	refs: (config, { configType }) => {
+		if (configType === 'DEVELOPMENT') {
+			return {
+				'react-maplibre': {
+					title: 'React MapLibreMap',
+					url: 'http://localhost:4400',
+				},
+				'deck-gl': {
+					title: 'Deck.gl',
+					url: 'http://localhost:4401',
+				},
+				'ra-geospatial': {
+					title: 'Ra Geospatial',
+					url: 'http://localhost:4402',
+				},
+				'three': {
+					title: 'Three',
+					url: 'http://localhost:4403',
+				},
+			};
+		}
+		return {
+			'react-maplibre': {
+				title: 'React MapLibreMap',
+				url: 'https://mapcomponents.github.io/react-map-components-maplibre/react-maplibre/',
+			},
+			'three': {
+				title: 'three',
+				url: 'https://mapcomponents.github.io/react-map-components-maplibre/three/',
+			},
+			'deck-gl': {
+				title: 'Deck.gl',
+				url: 'https://mapcomponents.github.io/react-map-components-maplibre/deck-gl/',
+			},
+			'ra-geospatial': {
+				title: 'React Admin Geospatial',
+				url: 'https://mapcomponents.github.io/react-map-components-maplibre/ra-geospatial/',
+			},
+		};
+	},
 };
 
 export default config;
