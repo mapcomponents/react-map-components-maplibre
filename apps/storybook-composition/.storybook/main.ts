a39ae18f307c073b4e944a6e11936883efae5995
jannik brack <jannik.brack@wheregroup.com>
2026-05-07 15:14:12 +0200

    Update Storybook configuration and TypeScript settings (storybook-composition)

--- apps/storybook-composition/.storybook/main.ts
+++ apps/storybook-composition/.storybook/main.ts
@@ -1,21 +1,10 @@
-// This file has been automatically migrated to valid ESM format by Storybook.
-import { createRequire } from "node:module";
-import { dirname, join } from 'node:path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 
-const require = createRequire(import.meta.url);
-
-
-function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, 'package.json')));
-}
-
 const config: StorybookConfig = {
 	stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-	addons: [],
 	framework: {
-		name: getAbsolutePath('@storybook/react-vite'),
+		name: "@storybook/react-vite",
 		options: {
 			builder: {
 				viteConfigPath: 'vite.config.ts',
@@ -23,50 +12,7 @@ const config: StorybookConfig = {
 		},
 	},
 	staticDirs: ['../public'],
-	refs: (config, { configType }) => {
-		if (configType === 'DEVELOPMENT') {
-			return {
-				'react-maplibre': {
-					title: 'React MapLibreMap',
-					url: 'http://localhost:4400',
-				},
-				'deck-gl': {
-					title: 'Deck.gl',
-					url: 'http://localhost:4401',
-				},
-				'ra-geospatial': {
-					title: 'Ra Geospatial',
-					url: 'http://localhost:4402',
-				},
-				'three': {
-					title: 'Three',
-					url: 'http://localhost:4403',
-				},
-			};
-		}
-		return {
-			'react-maplibre': {
-				title: 'React MapLibreMap',
-				url: 'https://mapcomponents.github.io/react-map-components-maplibre/react-maplibre/',
-			},
-			'three': {
-				title: 'three',
-				url: 'https://mapcomponents.github.io/react-map-components-maplibre/three/',
-			},
-			'deck-gl': {
-				title: 'Deck.gl',
-				url: 'https://mapcomponents.github.io/react-map-components-maplibre/deck-gl/',
-			},
-			'ra-geospatial': {
-				title: 'React Admin Geospatial',
-				url: 'https://mapcomponents.github.io/react-map-components-maplibre/ra-geospatial/',
-			},
-		};
-	},
+
 };
 
 export default config;
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/recipes/storybook/custom-builder-configs
--- apps/storybook-composition/.storybook/manager.js
+++ apps/storybook-composition/.storybook/manager.js
@@ -1,6 +1,10 @@
-import { addons } from "@storybook/addons";
+import { addons } from "storybook/manager-api";
 import wheregroupTheme from "./wheregroupTheme";
 
+addons.register('custom-panel', (api) => {
+	api.togglePanel(false);
+});
+
 addons.setConfig({
-  theme: wheregroupTheme,
+	theme: wheregroupTheme,
 });
