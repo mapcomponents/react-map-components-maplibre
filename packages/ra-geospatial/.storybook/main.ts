11966689ad4ad87e7a6e7fe4919e2117ae6c0d9a
jannik brack <jannik.brack@wheregroup.com>
2026-05-07 15:17:38 +0200

    Update Storybook configuration and TypeScript settings (ra-geospatial)

--- packages/ra-geospatial/.storybook/main.ts
+++ packages/ra-geospatial/.storybook/main.ts
@@ -1,10 +1,11 @@
-import { dirname, join } from 'node:path';
 import type { StorybookConfig } from '@storybook/react-vite';
+
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+	addons: ["@storybook/addon-docs"],
 	framework: {
-		name: getAbsolutePath("@storybook/react-vite"),
+		name: "@storybook/react-vite",
 		options: {
 			builder: {
 				viteConfigPath: 'vite.config.ts',
@@ -12,14 +13,17 @@ const config: StorybookConfig = {
 		},
 	},
 	staticDirs: ['../public'],
+
+	typescript: {
+		check: false,
+		reactDocgen: 'react-docgen-typescript',
+		reactDocgenTypescriptOptions: {
+			tsconfigPath: "./tsconfig.lib.json",
+			shouldExtractLiteralValuesFromEnum: true,
+			propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+		},
+	},
+
 };
 
 export default config;
-
-// To customize your Vite configuration you can use the viteFinal field.
-// Check https://storybook.js.org/docs/react/builders/vite#configuration
-// and https://nx.dev/recipes/storybook/custom-builder-configs
-
-function getAbsolutePath(value: string): any {
-	return dirname(require.resolve(join(value, "package.json")));
-}
--- packages/ra-geospatial/.storybook/manager.js
+++ packages/ra-geospatial/.storybook/manager.js
@@ -1,6 +1,6 @@
-import { addons } from "@storybook/addons";
+import { addons } from "storybook/manager-api";
 import wheregroupTheme from "./wheregroupTheme";
 
 addons.setConfig({
-  theme: wheregroupTheme,
+	theme: wheregroupTheme,
 });
