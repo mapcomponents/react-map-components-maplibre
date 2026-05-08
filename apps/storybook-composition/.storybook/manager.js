bf82c244bba0a2fdebc0832ab0b322099a7ef77f
jannik brack <jannik.brack@wheregroup.com>
2026-05-08 10:10:58 +0200

    Remove custom panel registration from Storybook manager

--- apps/storybook-composition/.storybook/manager.js
+++ apps/storybook-composition/.storybook/manager.js
@@ -1,10 +1,6 @@
 import { addons } from "storybook/manager-api";
 import wheregroupTheme from "./wheregroupTheme";
 
-addons.register('custom-panel', (api) => {
-	api.togglePanel(false);
-});
-
 addons.setConfig({
 	theme: wheregroupTheme,
 });
