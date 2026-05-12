098d01c4af507ae8e77edb1d684ddc63bfb01bb2
jannik brack <jannik.brack@wheregroup.com>
2026-05-12 12:16:40 +0200

    fix: enhance Cypress configuration to handle WebGL errors and improve browser launch options

--- packages/react-maplibre/cypress.config.ts
+++ packages/react-maplibre/cypress.config.ts
@@ -1,9 +1,31 @@
 import { nxComponentTestingPreset } from '@nx/react/plugins/component-testing';
 import { defineConfig } from 'cypress';
 
+const preset = nxComponentTestingPreset(__filename, {
+	bundler: 'vite',
+	buildTarget: 'react-maplibre:build',
+});
+
 export default defineConfig({
-	component: nxComponentTestingPreset(__filename, {
-		bundler: 'vite',
-		buildTarget: 'react-maplibre:build',
-	}),
+	component: {
+		...preset,
+		setupNodeEvents(on) {
+			on('before:browser:launch', (browser, launchOptions) => {
+				if (browser.family === 'chromium') {
+					launchOptions.args.push('--use-gl=angle');
+					launchOptions.args.push('--use-angle=swiftshader');
+					launchOptions.args.push('--ignore-gpu-blocklist');
+					launchOptions.args.push('--disable-gpu-sandbox');
+					launchOptions.args.push('--enable-webgl');
+				}
+				if (browser.name === 'electron') {
+					launchOptions.preferences.webPreferences = {
+						...launchOptions.preferences.webPreferences,
+						webgl: true,
+					};
+				}
+				return launchOptions;
+			});
+		},
+	},
 });
